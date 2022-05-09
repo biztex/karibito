4697a8ea87e8fad020cdd38b18a4da454a0f8ce4
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-05-09 22:07:18 +0900

    migrationの作成、userprofile

--- database/migrations/2022_05_09_074033_create_user_profiles_table.php
+++ database/migrations/2022_05_09_074033_create_user_profiles_table.php
@@ -11,10 +11,23 @@ return new class extends Migration
      *
      * @return void
      */
+
     public function up()
     {
         Schema::create('user_profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('first_name')->nullable(false);
+            $table->string('last_name')->nullable(false);
+            $table->tinyInteger('gender')->nullable(false)->comment('1.男性、2.女性');
+            $table->integer('prefecture')->nullable(false);
+            $table->date('birthday')->nullable(false);
+            $table->string('zip')->nullable(false);
+            $table->string('address')->nullable(false);
+            $table->text('introduction')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('cover')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
