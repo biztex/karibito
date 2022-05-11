7a44962ff3d724dfff1b4c76ba1942e36678caa2
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-05-11 19:07:54 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,7 +106,7 @@
                 console.log('Successful login for: ' + response.name);
                 // document.getElementById('status').innerHTML =
                 //   'Thanks for logging in, ' + response.name + '!';
-                window.location.href='https://karibito.4sis.site'
+                window.location.href='https://karibito.4sis.site/dashboard'
               });
             }
           
