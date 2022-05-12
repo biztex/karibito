24cd8f9844f62644ef361f808f313317ea97d869
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-05-12 17:51:15 +0900

    Update FacebookLoginController.php

--- app/Http/Controllers/FacebookLoginController.php
+++ app/Http/Controllers/FacebookLoginController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Socialite;
 // use App\Http\Controllers\User;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 
 class FacebookLoginController extends Controller
