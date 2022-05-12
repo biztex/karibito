6266d3534ed46d33dcae0155cd0858c85a2b12ac
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-05-12 16:25:36 +0900

    fb_login

--- app/Http/Controllers/FacebookLoginController.php
+++ app/Http/Controllers/FacebookLoginController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use App\Http\Controllers\Socialite;
+use Socialite;
 
 class FacebookLoginController extends Controller
 {
     public function getFacebookAuth()
     {
-        dd($user); // Facebookから取得した情報を表示
         return Socialite::driver('facebook')->redirect();
     }
 
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ use App\Http\Controllers\PublicationController;
 
 
 use App\Http\Controllers\GoogleLoginController;
+use App\Http\Controllers\FacebookLoginController;
+use App\Http\Controllers\Socialite;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -118,8 +121,8 @@ Route::get('/login/google', [GoogleLoginController::class, 'getGoogleAuth']);
 Route::get('/login/callback', [GoogleLoginController::class, 'authGoogleCallback']);
 
 // facebook login
-Route::get('login/facebook', [FacebookLoginController::class, 'getFacebookAuth']);
-Route::get('login/facebook/callback', [FacebookLoginController::class, 'authFacebookCallback']);
+Route::get('/login/facebook', [FacebookLoginController::class, 'getFacebookAuth']);
+Route::get('/login/facebook/callback', [FacebookLoginController::class, 'authFacebookCallback']);
 
 
 Route::prefix('sample')->group(function (){
