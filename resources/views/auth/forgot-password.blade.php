7a0caac4e30a1a2ca9be943d5b8a3aea939926b4
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-05-19 18:47:34 +0900

    components消したらめんどくさいことになりました。

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -11,10 +11,10 @@
         </div>
 
         <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+        {{-- <x-auth-session-status class="mb-4" :status="session('status')" /> --}}
 
         <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        {{-- <x-auth-validation-errors class="mb-4" :errors="$errors" /> --}}
         @if(!empty($error_msg))
         {{  $error_msg  }}
         @endif
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,10 +8,10 @@
         </x-slot>
 
         <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+        {{-- <x-auth-session-status class="mb-4" :status="session('status')" /> --}}
 
         <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        {{-- <x-auth-validation-errors class="mb-4" :errors="$errors" /> --}}
         {{-- @if(!is_null($error_msg))
             <div class="">
                 {{ $error_msg  }}
