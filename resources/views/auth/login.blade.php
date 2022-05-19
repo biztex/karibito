fc12692ef9668a5d9b718f6e7b3f94c1767f302b
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-05-19 18:54:43 +0900

    続き

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 <x-guest-layout>
-    <x-auth-card>
+    {{-- <x-auth-card> --}}
         <x-slot name="logo">
             {{-- <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
@@ -68,5 +68,5 @@
         <a href="/login/facebook">フェイスブックログイン</a>
 
         {{-- <fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button> --}}
-    </x-auth-card>
+    {{-- </x-auth-card> --}}
 </x-guest-layout>
