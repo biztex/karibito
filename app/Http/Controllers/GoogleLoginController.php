0e569ae3980b13970b1a6696d302061f8e947b1d
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-05-23 14:54:10 +0900

    Update GoogleLoginController.php

--- app/Http/Controllers/GoogleLoginController.php
+++ app/Http/Controllers/GoogleLoginController.php
@@ -22,7 +22,7 @@ class GoogleLoginController extends Controller
     public function authGoogleCallback()
     {
         $sns_user = Socialite::driver('google')->stateless()->user();
-        // dd($sns_user);
+        dd($sns_user);
         $user = User::where('google_id', $sns_user->id)->first();
         // $user = User::where('email', $google_user->email)->first();
         if ($user){
@@ -47,7 +47,7 @@ class GoogleLoginController extends Controller
                 $user = UserProfile::create([
                     'icon' => $sns_user->avator,
                 ]);
-                dd($user);
+                // dd($user);
             }
                     Auth::login($user);
                     return redirect()->route('user_profile.create');
