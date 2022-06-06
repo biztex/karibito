e193b4a53b8c93b5a06337d79440a6d9d0a498f0
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-06-06 15:47:39 +0900

    マイグレーションファイルの作成

---
+++ database/migrations/2022_06_06_151902_add_identification_to_user_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+            $table->string('identification')->nullable()->after('deleted_at');
+            $table->boolean('is_identify')->nullable()->after('identification');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+            $table->dropColumn('identification');
+            $table->dropColumn('is_identify');
+        });
+    }
+};
