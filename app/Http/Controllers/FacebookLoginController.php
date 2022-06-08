e2866370ef93ee67a8eb41329034f3079a96be32
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-06-08 14:50:02 +0900

    Update FacebookLoginController.php

--- app/Http/Controllers/FacebookLoginController.php
+++ app/Http/Controllers/FacebookLoginController.php
@@ -40,7 +40,7 @@ class FacebookLoginController extends Controller
         // コメントを書く
         // メソッドで小さく区切る、クラス内で呼び出して使用する
         if(is_null($sns_user->email)){ //未確認、開発環境で確認できなかった
-            return redirect()->route('register')->with('flash_alert', 'ログイン情報が登録されていません。');
+            return redirect()->route('register')->with('flash_alert', 'フェイスブックにログイン情報が登録されていません。');
         }
         // すでにFacebook登録済みじゃなかったらユーザーを登録する
         $user = User::where('facebook_id', $sns_user->id)->first();
