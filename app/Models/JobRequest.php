c0a0768e993ec92b0be7b4e925c5973de91a350a
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-06-15 11:23:08 +0900

    下書き・公開非公開 定数定義

--- app/Models/JobRequest.php
+++ app/Models/JobRequest.php
@@ -9,6 +9,22 @@ class JobRequest extends Model
 {
     use HasFactory;
 
+    const STATUS_PUBLISH = 1;
+    const STATUS_PRIVATE = 2;
+
+    const SALES_STATUS = [
+        self::STATUS_PUBLISH => '公開',
+        self::STATUS_PRIVATE => '非公開',
+    ];
+
+    const IS_DRAFT = 0;
+    const NOT_DRAFT = 1;
+    const DRAFT_STATUS = [
+        self::IS_DRAFT => '下書き',
+        self::NOT_DRAFT => '下書きでない',
+    ];
+
+
     /**
      * The attributes that are mass assignable.
      *
@@ -20,5 +36,11 @@ class JobRequest extends Model
     public function mProductChildCategory()
     {
         return $this->belongsTo(MProductChildCategory::class,'category_id');
+    }
+
+    // Userモデルとのリレーション
+    public function user()
+    {
+        return $this->belongsTo(JobRequest::class);
     }
 }
