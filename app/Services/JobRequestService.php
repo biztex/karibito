780378d482a2b8a54d36a69ee1045e01f59304de
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-06-15 21:31:05 +0900

    修正

--- app/Services/JobRequestService.php
+++ app/Services/JobRequestService.php
@@ -27,6 +27,22 @@ class JobRequestService
     }
 
     /**
+     * リクエスト編集
+     */
+    public function updateJobRequest(array $params, $job_request):JobRequest
+    {
+        $columns = ['category_id',  'prefecture_id', 'title', 'content',  'price',  'application_deadline',  'required_date',  'is_online',  'is_call'];
+
+            foreach($columns as $column){
+                $job_request->$column = $params[$column];
+            }
+            $job_request->is_draft = JobRequest::NOT_DRAFT;
+            $job_request->status = JobRequest::STATUS_PUBLISH;
+            $job_request->save();
+        return $job_request;
+    }
+
+    /**
      * 新規リクエスト下書き保存
      */
     public function storeDraftJobRequest(array $params):JobRequest
