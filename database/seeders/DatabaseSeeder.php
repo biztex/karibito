6cc57a9e9b32d2ac6ccffc55b84d10d9c3e99ea8
Tkinoshita <1bey1140@gmail.com>
2022-06-16 22:41:59 +0900

    AdminシーダーとUserシーダーと開発者ログイン

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
     /** 実行したいSeederをここに登録 */
     private const SEEDERS = [
         PrefectureSeeder::class,
+        MProductCategorySeeder::class,
+        MProductChildCategorySeeder::class
     ];
 
     /**
