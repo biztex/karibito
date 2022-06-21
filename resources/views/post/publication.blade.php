503ac68274fc00d5e01cc393af7c074102706e51
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-06-21 16:10:16 +0900

    変更

--- resources/views/post/publication.blade.php
+++ resources/views/post/publication.blade.php
@@ -5,8 +5,8 @@
 				<a href="{{ route('home') }}">ホーム</a>　>　<span>掲載一覧</span>
 			</div>
 		</div><!-- /.breadcrumb -->
-		<div class="btnFixed"><a href="{{ route('post') }}"><img src="img/common/btn_fix.svg" alt="投稿"></a></div>
-		
+		<div class="btnFixed"><a href="{{ route('product.index') }}"><img src="img/common/btn_fix.svg" alt="投稿"></a></div>
+
 		<div id="contents" class="otherPage">
 			<div class="inner02 clearfix">
 				<div id="main">
