cc58336a58dcc91af0f900db32ced8db7bd1064d
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-06-21 16:07:39 +0900

    途中

--- app/Http/Requests/ProductController/PreviewRequest.php
+++ app/Http/Requests/ProductController/PreviewRequest.php
@@ -25,14 +25,26 @@ class PreviewRequest extends FormRequest
     {
         return [
             'category_id' => 'nullable | integer | exists:m_product_child_categories,id',
-            'prefecture_id' => 'nullable | between:1,47',
-            'title' => 'nullable | string | max:30',
-            'content' => 'nullable | string | min:30 | max:3000 ',
-            'price' => 'nullable | integer | min:500 | max:9990000',
-            'application_deadline' => 'nullable | date | after_or_equal:tomorrow',
-            'required_date' => 'nullable | date | after_or_equal:application_deadline',
-            'is_online' => 'nullable | boolean',
-            'is_call' => 'nullable | boolean',
+            'prefecture_id' => 'nullable | integer | between:1,47',
+            'title' => 'required | string | max:30',
+            'content' => 'required | string | min:30 | max:3000 ',
+            'price' => 'required | integer | min:500 | max:9990000',
+            'is_online' => 'required | integer | boolean',
+            'number_of_day' => 'required | integer',
+            'is_call' => 'required | integer | boolean',
+            'number_of_sale' => ' required | integer',
         ];
     }
 }
+
+return [
+    'category_id' => 'nullable | integer | exists:m_product_child_categories,id',
+    'prefecture_id' => 'nullable | between:1,47',
+    'title' => 'nullable | string | max:30',
+    'content' => 'nullable | string | min:30 | max:3000 ',
+    'price' => 'nullable | integer | min:500 | max:9990000',
+    'application_deadline' => 'nullable | date | after_or_equal:tomorrow',
+    'required_date' => 'nullable | date | after_or_equal:application_deadline',
+    'is_online' => 'nullable | boolean',
+    'is_call' => 'nullable | boolean',
+];
