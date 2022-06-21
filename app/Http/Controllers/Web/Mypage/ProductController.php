520ebec384ffe2f9c7118af057dc10cd9d2c245d
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-06-21 15:08:52 +0900

    Update ProductController.php

--- app/Http/Controllers/Web/Mypage/ProductController.php
+++ app/Http/Controllers/Web/Mypage/ProductController.php
@@ -160,7 +160,7 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param int $id
+     * @param \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
     public function destroy(Product $product)
