47b1fd47316df52e6cea74cbf292dcceca907f93
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-07-07 08:42:21 +0900

    migrations

---
+++ database/migrations/2022_07_06_185717_create_product_proposals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_proposals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_chatroom_id')->constrained()->cascadeOnDelete()->comment('サービスやり取りID');
+            $table->foreignId('user_id')->constrained('users')->comment('提案したユーザーID');
+            $table->integer('price')->comment('提供価格')->nullable();
+            $table->boolean('is_purchese')->default(0)->comment('購入フラグ(0.未購入,1.購入)');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_proposals');
+    }
+};
