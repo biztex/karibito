85ea2384fda16c0158066087e4b555cdf881aabe
iwa0611 <daimoto7211@yahoo.co.jp>
2022-07-07 14:55:09 +0900

    telの型integerに変更。フォーム下のコメント追加

--- database/migrations/2022_07_07_110450_add_tel_to_users_table.php
+++ database/migrations/2022_07_07_110450_add_tel_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('tel')->nullable()->after('name');
+            $table->integer('tel')->nullable()->after('name');
         });
     }
 
