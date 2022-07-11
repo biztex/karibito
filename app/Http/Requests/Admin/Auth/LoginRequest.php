69c17e3c55bc3675d053d45dfbbfc97ce2e0a0d1
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-07-11 12:06:59 +0900

    ログイン修正

--- app/Http/Requests/Admin/Auth/LoginRequest.php
+++ app/Http/Requests/Admin/Auth/LoginRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Auth;
+namespace App\Http\Requests\Admin\Auth;
 
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
@@ -19,6 +19,12 @@ class LoginRequest extends FormRequest
     public function authorize()
     {
         return true;
+    }
+
+    protected function prepareForValidation()
+    {
+        // input name=idをemailとして処理する
+        $this->merge(['email' => $this->id]);
     }
 
     /**
