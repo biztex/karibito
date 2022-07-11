e094ec7a9bddcc80ad045efae2e5a7057a043b11
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-07-11 13:16:23 +0900

    修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3214,12 +3214,16 @@
 								<div class="newsList mt60">
 									<h2 class="hdM">カリビトからのお知らせ<a href="{{ route('news.index') }}" class="more">お知らせをもっと見る</a></h2>
 									<div class=box>
-                                        @foreach($news_list as $news)
-                                            <dl>
-                                                <dt>{{$news->created_at->format('Y/m/d')}}</dt>
-                                                <dd><a href="{{ route('news.show', $news->id) }}">{{$news->title}}</a></dd>
-                                            </dl>
-                                        @endforeach
+                                        @if(empty($news_list[0]))
+                                            <div>カリビトからのお知らせがありません。</div>
+                                        @else
+                                            @foreach($news_list as $news)
+                                                <dl>
+                                                    <dt>{{$news->created_at->format('Y/m/d')}}</dt>
+                                                    <dd><a href="{{ route('news.show', $news->id) }}">{{$news->title}}</a></dd>
+                                                </dl>
+                                            @endforeach
+                                        @endif
 									</div>
 								</div>
 							</div><!-- /#main -->
@@ -6411,12 +6415,16 @@
 								<div class="newsList mt60">
 									<h2 class="hdM">カリビトからのお知らせ<a href="{{ route('news.index') }}" class="more">お知らせをもっと見る</a></h2>
 									<div class=box>
-                                        @foreach($news_list as $news)
-										<dl>
-											<dt>{{$news->created_at}}</dt>
-											<dd><a href="{{ route('news.show', $news->id) }}">{{$news->title}}</a></dd>
-										</dl>
-                                        @endforeach
+                                        @if(empty($news_list[0]))
+                                            <div>カリビトからのお知らせがありません。</div>
+                                        @else
+                                            @foreach($news_list as $news)
+                                                <dl>
+                                                    <dt>{{$news->created_at->format('Y/m/d')}}</dt>
+                                                    <dd><a href="{{ route('news.show', $news->id) }}">{{$news->title}}</a></dd>
+                                                </dl>
+                                            @endforeach
+                                        @endif
 									</div>
 								</div>
 							</div><!-- /#main -->
