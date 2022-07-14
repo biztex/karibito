809867aa4e53822f0a11dadb27dad4f57c1f6135
Tkinoshita <1bey1140@gmail.com>
2022-07-14 13:48:10 +0900

    デザイン修正

--- resources/views/resume/skill_create.blade.php
+++ resources/views/resume/skill_create.blade.php
@@ -13,7 +13,7 @@
 					<div class="mypageWrap">
 						<div class="mypageSec04">
 							<p class="mypageHd02"><span>スキル・経歴</span></p>
-                            <form method="post" class="contactForm" enctype="multipart/form-data">
+                            <form method="post" enctype="multipart/form-data">
                             @csrf
                                 <div class="mypageItem">
                                     <p class="mypageHd03">スキル</p>
@@ -23,7 +23,7 @@
                                                 <div class="mypageEditList flexLineLeft01">
                                                     <p class="mypageEditHd">スキル名</p>
                                                     @error('name')<div class="alert alert-danger">{{ $message }}</div>@enderror
-                                                    <div class="mypageEditInput"><input type="text" name="name" placeholder="スキル名を入力してください"></div>
+                                                    <div class="mypageEditInput"><input type="text" name="name" placeholder="スキル名を入力してください" required></div>
                                                 </div>
                                                 <div class="mypageEditList flexLineRight01">
                                                     <p class="mypageEditHd">経験年数</p>
@@ -34,6 +34,22 @@
                                                             <option>2</option>
                                                             <option>3</option>
                                                             <option>4</option>
+                                                            <option>5</option>
+                                                            <option>6</option>
+                                                            <option>7</option>
+                                                            <option>8</option>
+                                                            <option>9</option>
+                                                            <option>10</option>
+                                                            <option>11</option>
+                                                            <option>12</option>
+                                                            <option>13</option>
+                                                            <option>14</option>
+                                                            <option>15</option>
+                                                            <option>16</option>
+                                                            <option>17</option>
+                                                            <option>18</option>
+                                                            <option>19</option>
+                                                            <option>20</option>
                                                         </select>
                                                         <span class="date_span">年</span>
                                                     </div>
