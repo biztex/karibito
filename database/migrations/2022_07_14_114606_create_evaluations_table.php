812dae047864b6e866a219ff1b552ce21bc74a66
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-07-19 20:00:10 +0900

    migration 修正

--- database/migrations/2022_07_14_114606_create_evaluations_table.php
+++ database/migrations/2022_07_14_114606_create_evaluations_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('evaluations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('chatroom_id')->constrained()->cascadeOnDelete()->comment('サービスやり取りID');
-            $table->foreignId('user_id')->constrained('users')->comment('評価したユーザーID');
-            $table->foreignId('target_user_id')->constrained('users')->comment('評価されたユーザーID');
+            $table->foreignId('chatroom_id')->comment('サービスやり取りID')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->comment('評価したユーザーID')->constrained('users');
+            $table->foreignId('target_user_id')->comment('評価されたユーザーID')->constrained('users');
             $table->double('star',2,1)->comment('評価(5:良い 2.5:普通 1.残念)');
             $table->text('text')->comment('コメント')->nullable();
             $table->softDeletes();
