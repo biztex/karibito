f38f78dbe2a4cf018a66f5dd016886725ec0f9df
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-07-19 18:57:47 +0900

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,12 +46,16 @@
 				<div class="inner">
 					<div class="box newsList">
 						<h2 class="hd">運営からのお知らせ</h2>
-						@foreach ($important_news_list as $important_news)
-							<dl>
-								<dt>{{$important_news->created_at->format('Y/m/d')}}</dt>
-								<dd><a href="{{ route('news.show', $important_news->id) }}">{{$important_news->title}}</a></dd>
-							</dl>
-						@endforeach
+						@if(empty($important_news_list[0]))
+							<div>運営からのお知らせは現在ありません。</div>
+						@else
+							@foreach ($important_news_list as $important_news)
+								<dl>
+									<dt>{{$important_news->created_at->format('Y/m/d')}}</dt>
+									<dd><a href="{{ route('news.show', $important_news->id) }}">{{$important_news->title}}</a></dd>
+								</dl>
+							@endforeach
+						@endif
 					</div>
 				</div>
 			</div>
