6e247a33bf8099936938db5d055feb345efe8669
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-07-22 15:40:03 +0900

    ヘッダー、サイドメニュー絞り込み

--- app/Http/Controllers/Web/OtherUser/ProductController.php
+++ app/Http/Controllers/Web/OtherUser/ProductController.php
@@ -6,9 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\MProductCategory;
 use App\Models\MProductChildCategory;
+use Illuminate\Http\Request;
+use App\Services\ProductService;
+
 
 class ProductController extends Controller
 {
+
+
+    private $product_service;
+
+    public function __construct(ProductService $product_service)
+    {
+        $this->product_service = $product_service;
+    }
 
         /**
      * Show the form for creating a new resource.
@@ -39,5 +50,23 @@ class ProductController extends Controller
         $parent_category_flg = 0;
 
         return view('product.index', compact('products', 'all_child_categories', 'child_category', 'child_category_id', 'parent_category_flg'));
+    }
+
+    public function search(Request $request)
+    {
+        $products = $this->product_service->searchProducts($request);
+
+        $prefecture_id = $request->prefecture_id;
+        $low_price = $request->low_price;
+        $high_price = $request->high_price;
+        $is_online = $request->is_online;
+        $age_period = $request->age_period;
+        // $all_child_categories = $child_category->mProductCategory->mProductChildCategory; //親かて、コレクション
+
+        // $child_category_id = $child_category->id;
+
+        // $parent_category_flg = 0;
+
+        return view('product.index', compact('products', 'prefecture_id', 'low_price', 'high_price', 'is_online', 'age_period'));
     }
 }
