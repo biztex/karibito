bd79de017270c4424d2f347f5a2b43488144a9d8
iwa0611 <daimoto7211@yahoo.co.jp>
2022-07-27 17:56:07 +0900

    basic認証をlog-viewerに実装

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Arcanedev\LogViewer\LogViewerServiceProvider::class,
-
+        Olssonm\VeryBasicAuth\VeryBasicAuthServiceProvider::class,
     ],
 
     /*
