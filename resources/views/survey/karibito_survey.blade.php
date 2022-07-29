269d8eb8e29698c58630dcbde0a34791106b0134
中澤 <104249781+naka-zawa@users.noreply.github.com>
2022-07-29 16:35:16 +0900

    アンケート評価星

--- resources/views/survey/karibito_survey.blade.php
+++ resources/views/survey/karibito_survey.blade.php
@@ -17,18 +17,20 @@
                     <form action="{{ route('survey.store', $chatroom) }}" method="POST" enctype="multipart/form-data">
 					@csrf
                         <div class="evaluation">
-                            <!-- star選択素材がなかったので仮で作ってます -->
-                            <div class="mypageEditInput flexLine02">
-                                <select name="star">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                            </div>
                             <p class="scroe"><span>アンケートを送信いただけますと<br>もれなくカリビトポイント<big>50PT</big>プレゼント！</span></p>
-                            <p class="stars">カリビトを５つ星で評価ください。<img src="/img/cancel/ico_5stars.svg" alt=""></p>
+                            <p class="stars">カリビトを５つ星で評価ください</p>
+                            <div class="rate-form">
+                                <input id="star5" type="radio" name="star" value="5">
+                                <label for="star5">★</label>
+                                <input id="star4" type="radio" name="star" value="4">
+                                <label for="star4">★</label>
+                                <input id="star3" type="radio" name="star" value="3">
+                                <label for="star3">★</label>
+                                <input id="star2" type="radio" name="star" value="2">
+                                <label for="star2">★</label>
+                                <input id="star1" type="radio" name="star" value="1">
+                                <label for="star1">★</label>
+                            </div>
                             <div class="write">
                                 <p>評価コメントを記入しましょう</p>
                                 @error('comment')<div class="alert alert-danger">{{ $message }}</div>@enderror
