7a2aaff66a958a367cccdc46009aa3634e8a752d
iwa0611 <daimoto7211@yahoo.co.jp>
2022-08-01 09:29:07 +0900

    各ビューにコンポーネント追加

--- resources/views/admin/survey/index.blade.php
+++ resources/views/admin/survey/index.blade.php
@@ -3,7 +3,8 @@
     <x-admin.flash_msg/>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-12">
+            <x-admin.side_menu/>
+            <div class="col-md-10">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb border bg-white shadow-sm">
 {{--                        <li class="breadcrumb-item"><a href="{{ route('admin.home.index') }}">Home</a></li>--}}
@@ -30,7 +31,7 @@
                             </thead>
                             <tbody>
                                 @foreach($karibito_survey as $value)
-                                    <tr> 
+                                    <tr>
                                         <td class="px-2">{!! $value->id !!}</td>
                                         <td class="px-2">{!! $value->user_id !!}</td>
                                         <td class="px-2">{!! $value->star !!}</td>
