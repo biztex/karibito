b529aad08f706e8b93579c12a974ce1e6f97a411
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-08-02 10:51:24 +0900

    other-user

--- resources/views/other-user/evaluation.blade.php
+++ resources/views/other-user/evaluation.blade.php
@@ -21,7 +21,7 @@
 						<h2 class="subPagesHd">評価一覧</h2>
 						<div class="evaluationStar">
 							<span>総評</span>
-							<div class="evaluate three"></div>
+							<x-parts.evaluation-star :star='$user->avg_star'/>
 						</div>
 						<div class="subPagesTab tabWrap">
 							<ul class="tabLink">
@@ -101,34 +101,7 @@
 						</div>
 					</div>
 				</div><!-- /#main -->
-				<aside id="side" class="">
-					<div class="box seller">
-						<h3>スキル出品者</h3>
-						@if(null !== $user->userProfile->icon)
-							<a href="#" class="head"><img src="{{ asset('/storage/'.$user->userProfile->icon) }}" alt="" style="width: 120px;height: 120px;object-fit: cover;"></a>
-						@else
-							<a href="#" class="head"><img src="/img/mypage/no_image.jpg" alt="" style="width: 120px;height: 120px;object-fit: cover;"></a>
-						@endif
-						<!-- <p class="login">最終ログイン：8時間前</p> -->
-
-						<p class="introd"><span>{{$user->name}}</span><br>({{\App\Models\UserProfile::GENDER[$user->userProfile->gender]}}/ {{$age}}/ {{$user->userProfile->prefecture->name}})</p>
-
-
-						<!-- <div class="evaluate three"></div> -->
-						<p class="check"><a href="#">本人確認済み</a></p>
-						<!-- <p class="check"><a href="#">機密保持契約(NDA) 可能</a></p> -->
-                        @if (\Auth::id() !== $user->id)
-						    <div class="blogDtOtherBtn">
-							<a href="#" class="followA">フォローする</a>
-							@if(empty($dmrooms))
-								<a href="{{ route('dm.create',$user->id) }}">メッセージを送る</a>
-							@else
-								<a href="{{ route('dm.show',$dmrooms->id) }}">メッセージを送る</a>
-							@endif
-                        @endif
-						</div>
-					</div>
-				</aside><!-- /#side -->
+				@include('other-user.parts.side')
 			</div><!--inner-->
 		</div><!-- /#contents -->
         <x-hide-modal/>
