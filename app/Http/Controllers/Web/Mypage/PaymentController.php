25422a334048a0883b5b81f2c527c4e1a8d24932
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-08-02 12:36:41 +0900

    migration作成

---
+++ app/Http/Controllers/Web/Mypage/PaymentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Web\Mypage;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    //
+}
