a63ecb3c7c3c0a07c092203d5ee7c571d49c0daf
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-08-10 16:22:31 +0900

    Update index.blade.php

--- resources/views/mypage/coupon/index.blade.php
+++ resources/views/mypage/coupon/index.blade.php
@@ -15,12 +15,18 @@
 					<div class="subPagesWrap">
 						<h2 class="subPagesHd">クーポン</h2>
 						<div class="couponBox">
-							<dl class="couponHd">
-								<dt><img src="img/mypage/img_notice01.png" alt=""></dt>
-								<dd>ご利用いただけるクーポンが発行されました。</dd>
-							</dl>
+							@if ($user_coupons->isNotEmpty())
+								<dl class="couponHd">
+									<dt><img src="img/mypage/img_notice01.png" alt=""></dt>
+									<dd>ご利用いただけるクーポンが発行されました。</dd>
+								</dl>
+							@endif
 							<div class="couponCont">
-								<p class="couponNotice">会計時にクーポン番号を入力してください。</p>
+								@if ($user_coupons->isNotEmpty())
+									<p class="couponNotice">会計時にクーポン番号を入力してください。</p>
+								@else
+									<p class="">現在クーポンはまだありません。</p>
+								@endif
 								@foreach ($user_coupons as $user_coupon)
 									<div class="couponItem">
 										<dl class="couponDl">
