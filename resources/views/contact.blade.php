bf431e069f315594e58aa13471721ec3176a19a1
iwa0611 <daimoto7211@yahoo.co.jp>
2022-08-19 14:29:18 +0900

    ログイン時お問い合わせフォームに名前とアドレスが入る様に修正

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,8 +16,8 @@
                             <p class="contactTop">当サービスへのお問い合わせは、下記フォームにご入力いただき、送信いただきますようお願いいたします。</p>
                             <form action="{{ route('contact') }}" method="post">
                                 @csrf
-                                <div class="labelName"><input type="text" name="name" placeholder="名前" required></div>
-                                <div class="labelMail"><input type="email" name="mail" placeholder="メールアドレス" required></div>
+                                <div class="labelName"><input type="text" name="name" placeholder="名前" @if(\Auth::User()) value="{{ \Auth::User()->name }}" @endif required></div>
+                                <div class="labelMail"><input type="email" name="mail" placeholder="メールアドレス" @if(\Auth::User()) value="{{ \Auth::User()->email }}" @endif required></div>
                                 <div class="labelType"><select name="type">
                                     @foreach (App\Models\ContactMailHistory::CONTACT_TYPES as $i => $option)
                                         <option value='{{ $i }}'>{{ $option }}</option>
