86265a14cfb674e5e886e83862f2440de0b36e0f
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-08-20 15:32:23 +0900

    評価の出品者・購入者タグ追加

--- resources/views/components/parts/evaluation.blade.php
+++ resources/views/components/parts/evaluation.blade.php
@@ -7,7 +7,7 @@
         @endif
     </div>
     <div class="info">
-        <p class="name">{{ $value->user->name }}</p>
+        <p class="name">{{ $value->user->name }}<span>@if($value->user_id === $value->chatroom->seller_user_id)出品者@else 購入者@endif</span></p>
         <div class="cont">
             <p class="date">{{ $value->created_at->format('Y年m月d日') }}</p>
             <p class="txt" style="word-wrap: break-word;">{!!nl2br($value->text)!!}</p>
