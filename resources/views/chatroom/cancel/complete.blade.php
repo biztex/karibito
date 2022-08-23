37a1da4d04b10c38ddd28f386d354417464fdbcd
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-08-23 15:24:44 +0900

    Update complete.blade.php

--- resources/views/chatroom/cancel/complete.blade.php
+++ resources/views/chatroom/cancel/complete.blade.php
@@ -21,11 +21,11 @@
 							<p class="stepDot"></p>
 							<p class="stepTxt">チャット開始</p>
 						</li>
-						<li @if($value->status >= App\Models\Chatroom::STATUS_PROPOSAL) class="is_active" @endif>
+						<li @if($purchased_cancel->purchase->chatroom->status >= App\Models\Chatroom::STATUS_PROPOSAL) class="is_active" @endif>
 							<p class="stepDot"></p>
 							<p class="stepTxt">契約</p>
 						</li>
-						<li @if($value->status >= App\Models\Chatroom::STATUS_BUYER_EVALUATION) class="is_active" @endif>
+						<li @if($purchased_cancel->purchase->chatroom->status >= App\Models\Chatroom::STATUS_BUYER_EVALUATION) class="is_active" @endif>
 							<p class="stepDot"></p>
 							<p class="stepTxt">評価</p>
 						</li>
