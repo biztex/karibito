f331c8504331a44b9be4ce6f37dbe0951df91be9
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-08-23 15:11:42 +0900

    パンくず修正

--- resources/views/chatroom/cancel/complete.blade.php
+++ resources/views/chatroom/cancel/complete.blade.php
@@ -1,15 +1,35 @@
 <x-layout>
     <article>
 		<div id="breadcrumb">
-			<div class="inner">
-				<a href="{{ route('home') }}">ホーム</a>　>　<a href="#">やること</a>　>　<span>お支払い手続き</span>
-			</div>
-		</div><!-- /.breadcrumb -->
+            <div class="inner">
+                    <a href="{{ route('home') }}">ホーム</a>　>　
+                    <a href="{{ route('chatroom.index') }}">やり取り一覧</a>　>　
+				@if($purchased_cancel->purchase->chatroom->reference === 'App\Models\Product')
+                    <a href="{{ route('product.show', $purchased_cancel->purchase->chatroom->reference_id) }}">{{$purchased_cancel->purchase->chatroom->reference->title}}</a>　>　
+                @else <!-- JobRequest-->
+                    <a href="{{ route('job_request.show', $purchased_cancel->purchase->chatroom->reference_id) }}">{{$purchased_cancel->purchase->chatroom->reference->title}}</a>　>　
+                @endif
+                    <span>キャンセル手続き</span>
+            </div>
+        </div><!-- /.breadcrumb -->
 		<div id="contents" style="margin-bottom:35px;">
 			<div class="cancelWrap">
 				<div class="inner inner05">
-					
-					<x-parts.chatroom-step :value="$purchased_cancel->purchase->chatroom"/>
+					{{-- 修正必要 --}}
+					<ul id="chatroomStepUl" class="stepUl">
+						<li class="is_active">
+							<p class="stepDot"></p>
+							<p class="stepTxt">チャット開始</p>
+						</li>
+						<li @if($value->status >= App\Models\Chatroom::STATUS_PROPOSAL) class="is_active" @endif>
+							<p class="stepDot"></p>
+							<p class="stepTxt">契約</p>
+						</li>
+						<li @if($value->status >= App\Models\Chatroom::STATUS_BUYER_EVALUATION) class="is_active" @endif>
+							<p class="stepDot"></p>
+							<p class="stepTxt">評価</p>
+						</li>
+					</ul>
 
 					<div class="cancelTitle">
 						<h2>キャンセルの申請を承認いたしました</h2>
