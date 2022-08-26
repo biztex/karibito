44a3276c89b40cbf5f2a0307b9f8ab3866d7738a
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-08-26 17:48:58 +0900

    footer組込

---
+++ resources/views/components/footer/about-features.blade.php
@@ -0,0 +1,22 @@
+<div class="aboutFeatures">
+    <div class="inner">
+        <h2 class="hdM">カリビトの特徴</h2>
+        <div class="slider">
+            <img src="/img/feature/グループ 20248.svg" alt="">
+            <img src="/img/feature/グループ 20250.svg" alt="">
+            <img src="/img/feature/グループ 20252.svg" alt="">
+            <img src="/img/feature/グループ 20254.svg" alt="">
+            <img src="/img/feature/グループ 20257.svg" alt="">
+            <img src="/img/feature/グループ 20259.svg" alt="">
+            <img src="/img/feature/グループ 20261.svg" alt="">
+            <img src="/img/feature/グループ 20266.svg" alt="">
+            <img src="/img/feature/グループ 20269.svg" alt="">
+            <img src="/img/feature/グループ 20271.svg" alt="">
+            <img src="/img/feature/グループ 20273.svg" alt="">
+            <img src="/img/feature/グループ 20275.svg" alt="">
+            <img src="/img/feature/グループ 20279.svg" alt="">
+            <img src="/img/feature/グループ 20280.svg" alt="">
+            <img src="/img/feature/グループ 20281.svg" alt="">
+        </div>
+    </div>
+</div>
