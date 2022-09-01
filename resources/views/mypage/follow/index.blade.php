011d3bc0ccc8d3eafe9f813eb2eaa72257648757
iwa0611 <daimoto7211@yahoo.co.jp>
2022-09-01 17:31:38 +0900

    css調整。画面調整

--- resources/views/mypage/follow/index.blade.php
+++ resources/views/mypage/follow/index.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
-    <body id="estimate" class="friends-page">
+    <div id="friends">
+    <body id="estimate">
         <x-parts.post-button/>
         <x-parts.ban-msg/>
         <article>
@@ -84,68 +85,9 @@
 				</div><!-- /#main -->
                 <x-side-menu/>
 			</div><!--inner-->
-			<div class="aboutFeatures">
-				<div class="inner">
-					<h2 class="hdM">カリビトの特徴</h2>
-					<div class="slider">
-						<div><div class="item hlg01">
-							<h3 class="tit">カリビトチャット</h3>
-							<p class="ico ico_feat01"></p>
-							<p>条件交渉でチャットができます。<br>また、PDFや画像ファイルを<br>送信する事も可能です。</p>
-						</div></div>
-						<div><div class="item hlg01">
-							<h3 class="tit">検索機能</h3>
-							<p class="ico ico_feat02"></p>
-							<p>あなたの見たい情報を簡単に探せる<br>ために検索機能があります。<br>条件を入力するだけで簡単です。</p>
-						</div></div>
-						<div><div class="item hlg01">
-							<h3 class="tit">シェア機能</h3>
-							<p class="ico ico_feat03"></p>
-							<p>いいなと思った仕事をSNSを使って<br>シェアすることができます。<br>仲の良いメンバーに知らせる時に便利。</p>
-						</div></div>
-						<div><div class="item hlg01">
-							<h3 class="tit">お気に入り機能</h3>
-							<p class="ico ico_feat04"></p>
-							<p>気になる仕事や人材を<br>お気に入り登録をする事で<br>分からなくなることがありません。</p>
-						</div></div>
-						<div><div class="item hlg01">
-							<h3 class="tit">カリビトチャット</h3>
-							<p class="ico ico_feat01"></p>
-							<p>条件交渉でチャットができます。<br>また、PDFや画像ファイルを<br>送信する事も可能です。</p>
-						</div></div>
-					</div>
-				</div>
-			</div>
-			<div class="downloadWrap">
-				<div class="inner">
-					<div class="img"><img src="img/common/img_download.png" srcset="img/common/img_download.png 1x, img/common/img_download@2x.png 2x" alt=""><a href="#" target="_blank" class="logo sp"><img src="img/common/ico_sns_logo.svg" alt="LOGO"></a></div>
-					<div class="info">
-						<h2 class="tit">さあ、はじめよう！<span class="foucs">今すぐ無料ダウンロード！</span></h2>
-						<div class="links">
-							<a href="#" target="_blank" class="logo pc"><img src="img/common/ico_sns_logo.svg" alt="LOGO"></a>
-							<a href="#" target="_blank"><img src="img/common/btn_app.svg" alt="App Store"></a>
-							<a href="#" target="_blank"><img src="img/common/btn_google.svg" alt="Google"></a>
-						</div>
-					</div>
-				</div>
-			</div>
 		</div><!-- /#contents -->
 	</article>
+    <x-hide-modal/>
+    </body>
+    </div>
 </x-layout>
-</body>
-</div><!-- /#wrapper -->
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/jquery.matchHeight-min.js"></script>
-<script type="text/javascript" src="js/jquery.biggerlink.min.js"></script>
-<script type="text/javascript" src="js/slick.js"></script>
-<script type="text/javascript" src="js/jquery-ui.min.js"></script>
-<script type="text/javascript" src="js/jquery.ui.datepicker-ja.min.js"></script>
-<script type="text/javascript" src="js/jquery.fancybox.js"></script>
-<script type="text/javascript" src="js/common.js"></script>
-<script type="text/javascript">
-$(function(){
-
-});
-</script>
-</body>
-</html>
