bcb6bc1623b2118b73cb1f2b17e3f74d3924a6d2
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-09-14 04:50:30 +0900

    stripe処理

--- app/Libraries/Payment/PaymentInterface.php
+++ app/Libraries/Payment/PaymentInterface.php
@@ -7,43 +7,20 @@ namespace App\Libraries\Payment;
  */
 interface PaymentInterface
 {
-    /**
-     * 顧客カードの決済の実行
-     * @param string $card_id
-     * @param string $customer_id
-     * @param int $amount
-     * @param string $currency
-     * @return string $charge_id
-     */
-    // public function createCustomerCharge(string $card_id, string $customer_id, int $amount, string $currency): string;
 
     /**
-     * 決済の実行
-     * @param string $token
-     * @param int $amount
-     * @param string $currency
-     * @return string charge_id
+     * クレカ一覧取得
+     * @param string $customer_id
+     * @param int $limit
+     * @return array $cards
      */
-    // public function createCharge(string $token, int $amount, string $currency): string;
+    public function getCardList(string $customer_id, int $limit): array;
 
     /**
      * 顧客登録
      * @return string $customer_id
      */
     public function createCustomer(): string;
-
-    /**
-     * 顧客情報取得
-     * @return string $customer_id
-     */
-    public function getCustomer(): string;
-    
-    /**
-     * カードトークン発行
-     * @param array $params
-     * @return string $token
-     */
-    // public function createToken(array $params): string;
 
     /**
      * クレカ登録
@@ -54,21 +31,10 @@ interface PaymentInterface
     public function createCard(string $customer_id, string $token): void;
 
     /**
-     * クレカ情報取得
-     * @param string $customer_id
-     * @param string $payjp_card_id
-     * @return array $card
+     * 顧客情報取得
+     * @return string $customer_id
      */
-    // public function getCard(string $customer_id, string $payjp_card_id): object|array;
-
-    /**
-     * クレカ一覧取得
-     * @param string $customer_id
-     * @param int $limit
-     * @param int $offset
-     * @return array $cards
-     */
-    public function getCardList(string $customer_id, int $limit): array;
+    public function getCustomer(): string;
 
     /**
      * クレカ削除
@@ -76,12 +42,45 @@ interface PaymentInterface
      * @param string $card_id
      * @return void
      */
-    public function destroyCard(string $customer_id, string $card_id);
+    public function destroyCard(string $customer_id, string $card_id): void;
 
     /**
-     * 全額返金
-     * @param string $payjp_charge_id
-     * @return void
+     * トークンからカード情報取得
+     * @param string $token
+     * @return object|array
      */
-    // public function refundPayment(string $payjp_charge_id);
+    public function getToken(string $token): object|array;
+
+    /**
+     * クレカ情報取得
+     * @param string $customer_id
+     * @param string $card_id
+     * @return object|array
+     */
+    public function getCard(string $customer_id, string $card_id): object|array;
+
+    /**
+     * 支払い処理(一時決済)
+     * @param int $amount
+     * @param string $token
+     * @return mixed
+     */
+    public function createChargeByToken(int $amount, string $token): mixed;
+
+    /**
+     * 支払い処理(登録カード使用)
+     * @param int $amount
+     * @param string $customer_id
+     * @param string $card_id
+     * @return mixed
+     */
+    public function createChargeByCustomer(int $amount, string $customer_id, string $card_id): mixed;
+
+     /**
+     * 全額返金
+     * @param string $stripe_charge_id
+     * @return string $refund_id
+     */
+    public function refundPayment(string $stripe_charge_id): string;
+    
 }
