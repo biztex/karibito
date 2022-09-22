b32027c4ed097b591219cbac8928634c7e95c559
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-09-22 18:43:21 +0900

    usernotificationの修正

--- database/migrations/2022_09_06_181136_change_user_notifications_content_nullabe.php
+++ database/migrations/2022_09_06_181136_change_user_notifications_content_nullabe.php
@@ -14,9 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::table('user_notifications', function (Blueprint $table) {
-            $table->unsignedInteger("reference_id")->after('user_id'); // reference_id, reference_typeを作成
-            $table->string("reference_type")->after('reference_id');
-            $table->index(["reference_id", "reference_type"]);
+            // $table->unsignedInteger("reference_id")->after('user_id'); // reference_id, reference_typeを作成
+            // $table->string("reference_type")->after('reference_id');
+            // $table->index(["reference_id", "reference_type"]);
             $table->text('content')->comment('詳細')->change()->nullable();
         });
     }
@@ -29,8 +29,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('user_notifications', function (Blueprint $table) {
-            $table->dropColumn('reference_type');
-            $table->dropColumn('reference_id');
+            // $table->dropColumn('reference_type');
+            // $table->dropColumn('reference_id');
             $table->dropColumn('content');
         });
     }
