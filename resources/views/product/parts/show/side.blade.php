ef38893ef2f93ee40249c86191889e68093e9d08
s-iwkami5 <siwakami@minna-systems.co.jp>
2022-09-22 19:27:30 +0900

    Update side.blade.php

--- resources/views/product/parts/show/side.blade.php
+++ resources/views/product/parts/show/side.blade.php
@@ -10,9 +10,9 @@
             <h3>カリビト安心への取り組み</h3>
             <p>報酬は取引前に事務局に支払われ、評価・完了後に振り込まれます。利用規約違反や少しでも不審な内容のサービスやリクエストやユーザーがあった場合は通報してください。</p>
         </div>
-        {{-- <p class="specialtyBtn share"><span>この情報をシェアする</span></p> --}}
+        <p class="specialtyBtn share"><span>この情報をシェアする</span></p>
     </div>
 
-    {{-- <x-parts.box-seller :user='$product->user'/> --}}
+    <x-parts.box-seller :user='$product->user'/>
 
 </aside>
