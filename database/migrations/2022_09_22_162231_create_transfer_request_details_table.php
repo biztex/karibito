af3494b140ec0d87e2d20729db9843a3caba352a
Gabby3025 <102625619+Gabby3025@users.noreply.github.com>
2022-09-26 12:50:29 +0900

    振込申請内訳テーブル

---
+++ database/migrations/2022_09_22_162231_create_transfer_request_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfer_request_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transfer_request_id')->constrained();
+            $table->foreignId('profit_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfer_request_details');
+    }
+};
