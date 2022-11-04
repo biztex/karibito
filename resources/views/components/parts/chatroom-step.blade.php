35373739efaa9afa2e8c8bec361d653df63c9dee
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-11-04 10:16:45 +0900

    提案した段階で契約の表示になっていたのを交渉中に変更

--- resources/views/components/parts/chatroom-step.blade.php
+++ resources/views/components/parts/chatroom-step.blade.php
@@ -3,7 +3,7 @@
         <p class="stepDot"></p>
         <p class="stepTxt">交渉中</p>
     </li>
-    <li @if($value->status >= App\Models\Chatroom::STATUS_PROPOSAL) class="is_active" @endif>
+    <li @if($value->status >= App\Models\Chatroom::STATUS_PROPOSAL && $value->status >2) class="is_active" @endif>
         <p class="stepDot"></p>
         <p class="stepTxt">契約</p>
     </li>
