07d0aecd885dd926d8851daa83061ab7d63b7af7
Cyawanmushiman <xiaochuanyingsi@gmail.com>
2022-11-08 13:20:47 +0900

    退会者がしたお気に入りは残す

--- app/Services/FavoriteService.php
+++ app/Services/FavoriteService.php
@@ -8,17 +8,21 @@ use App\Models\JobRequest;
 
 class FavoriteService
 {
-    // Favoriteテーブルから退会者に関係する商品とリクエストを削除する
+    // 退会した人がいいねしたレコードは消さない。
+    // 退会した人の商品を他人がいいねしたレコードを消す。= 退会した人のreference_idのレコードを消す。
     public function deleteFavorites($user)
     {
-        Favorite::where('user_id',$user->id)->delete();
         $productIds = Product::where('user_id',$user->id)->select('id')->get();
         $jobRequestIds = JobRequest::where('user_id',$user->id)->select('id')->get();
         foreach($productIds as $productId){
-            Favorite::where('reference_id',$productId->id)->delete();
+            Favorite::where('reference_id',$productId->id)
+                    ->where('user_id', '!=', $user->id)
+                    ->delete();
         }
         foreach($jobRequestIds as $jobRequestId){
-            Favorite::where('reference_id',$jobRequestId->id)->delete();
+            Favorite::where('reference_id',$jobRequestId->id)
+                    ->where('user_id', '!=', $user->id)
+                    ->delete();
         }
     }
 }
