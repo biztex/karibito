57a58072d9f04f8c0ed29d213535727b06c92b9f
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-11-17 14:19:08 +0900

    退会後のメールの処理

---
+++ app/Mail/User/WithdrawMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class WithdrawMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to(\Auth::user()->email)
+            ->subject('【カリビト】[重要]退会完了のお知らせ')
+            ->text('mail.text.user.withdraw_mail');
+    }
+}
