a2d26f394e6cf82462e8d5e821dc10f1e02a1fd6
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-11-24 18:03:36 +0900

    if分岐追加

--- resources/views/setting/sub_email/create.blade.php
+++ resources/views/setting/sub_email/create.blade.php
@@ -20,7 +20,15 @@
 											@enderror
 										<p><input type="text" name="sub_email" value="@if (!isset($user->sub_email)) {{ old('sub_email') }} @else {{ old('sub_email', $user->sub_email) }} @endif "></p>
 									</div>
-										{{-- TODO:後ほどif文作成 --}}
+									@if (!isset($user->sub_email))
+										{{-- TODO:後ほどインデントを整形--}}
+										<ul class="loginFormBtn">
+											<li><input type="submit" class="submit loading-disabled" value="登録する"></li>
+										</ul>
+										<ul class="loginFormBtn">
+											<li><a href="{{ route('setting.index') }}">戻る</a></li>
+										</ul>
+									@else
 										<ul class="loginFormBtn">
 											<li><input type="submit" class="submit loading-disabled" value="登録する"></li>
 										</ul>
@@ -28,12 +36,13 @@
 											<li><a href="{{ route('setting.index') }}">戻る</a></li>
 										</ul>
 								</form>
-                                <form action="{{ route('sub_mail_destroy') }}" method="post"> {{-- 削除form --}}
-                                    @csrf
-                                    <ul class="loginFormBtn">
-                                        <li><input type="submit" style="background-color: crimson" class="submit loading-disabled" value="削除する"></li>
-                                    </ul>
-                                </form> {{-- 削除form --}}
+									<form action="{{ route('sub_mail_destroy') }}" method="post"> {{-- 削除form --}}
+										@csrf
+										<ul class="loginFormBtn">
+											<li><input type="submit" style="background-color: crimson" class="submit loading-disabled" value="削除する"></li>
+										</ul>
+									</form> {{-- 削除form --}}
+									@endif
 							</div>
 						</div>
 					</div><!-- /#main -->
