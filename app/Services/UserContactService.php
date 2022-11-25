3695d045ec1e7b2db19e546faf9b5d2b21c76885
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-11-25 12:27:25 +0900

    cc追加

--- app/Services/UserContactService.php
+++ app/Services/UserContactService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Mail\User\ContactMail;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
 
 class UserContactService
 {
@@ -12,7 +13,13 @@ class UserContactService
      */
     public function sendMail($request)
     {
-        Mail::to($request->mail)
-            ->send(new ContactMail($request));
+        if (!isset(Auth::user()->sub_email)) {
+            Mail::to($request->mail)
+                ->send(new ContactMail($request));
+        } else {
+            Mail::to($request->mail)
+                ->cc(Auth::user()->sub_email)
+                ->send(new ContactMail($request));
+        }
     }
 }
