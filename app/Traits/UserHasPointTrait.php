18c1e161e10bcb436aa2bd5e4458cf3604c189ad
小川英嗣 <ogawaeiji@ogawaeijiMacBook-Pro.local>
2022-11-30 21:45:17 +0900

    有効期限の古いポイントから使う処理

--- app/Traits/UserHasPointTrait.php
+++ app/Traits/UserHasPointTrait.php
@@ -17,19 +17,44 @@ trait UserHasPointTrait
     public function userHasPoint()
     {
         $today = date('Y-m-d');
-        $user_get_point = UserGetPoint::where([
-            ['user_id', '=', \Auth::id()],
-            ['deadline', '>=', $today],
-        ])->get()->sum('point'); //期限切れではないポイントの合計を取得
+        
+        // 有効期限内、かつ、有効期限が昇順の取得ポイントのコレクション
+        $get_points = UserGetPoint::where([
+                            ['user_id', '=', \Auth::id()],
+                            ['deadline', '>=', $today],
+                        ])
+                        ->orderBy('deadline', 'asc')
+                        ->select('point')
+                        ->get();
 
-        $user_use_point = UserUsePoint::where([
-            ['user_id', '=', \Auth::id()],
-            ['deleted_at', '=', null],
-        ])->get()->sum('point'); //期限切れではないポイントの合計を取得
 
-        $user_has_point = $user_get_point - $user_use_point;
+        // 総取得ポイント 15000
+        $has_point = $get_points->sum('point'); // 7000
 
-        return $user_has_point;
+        // 総使用ポイント 3800
+        $used_point = UserUsePoint::where([
+                            ['user_id', '=', \Auth::id()],
+                            ['deleted_at', '=', null],
+                        ])
+                        ->get()
+                        ->sum('point'); //期限切れではないポイントの合計を取得 2000ポイント
+        
+        // 古いポイントから差し引いていく
+        foreach($get_points as $key => $get_point_object) {
+            $get_point = $get_point_object->point;
+
+            if ($get_point <= $used_point) {
+                // used_point更新
+                $used_point -= $get_point;
+                // トータルポイント更新
+                $has_point -= $get_point;
+            }else{
+                $has_point -= $used_point;
+                break;
+            }
+        }
+
+        return $has_point;
     }
 
 }
