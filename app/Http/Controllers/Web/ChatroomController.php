4a99abe280d8144361daf26020b69857f5908978
小川英嗣 <ogawaeiji@ogawaeijiMacBook-Pro.local>
2022-12-05 13:42:44 +0900

    DMとチャットに電話番号のバリデーション追加　その他

--- app/Http/Controllers/Web/ChatroomController.php
+++ app/Http/Controllers/Web/ChatroomController.php
@@ -127,7 +127,8 @@ class ChatroomController extends Controller
         $chatroom = \DB::transaction(function () use ($request, $product) {
             $chatroom = $this->chatroom_service->startChatroomProduct($product);
             $this->chatroom_message_service->storeNormalMessage($request->all(), $chatroom);
-
+            $this->user_notification_service->storeUserNotificationMessage($chatroom);
+            
             return $chatroom;
         });
         return redirect()->route('chatroom.show', $chatroom->id);
@@ -145,6 +146,7 @@ class ChatroomController extends Controller
         $chatroom = \DB::transaction(function () use ($request, $job_request) {
             $chatroom = $this->chatroom_service->startChatroomJobRequest($job_request);
             $this->chatroom_message_service->storeNormalMessage($request->all(), $chatroom);
+            $this->user_notification_service->storeUserNotificationMessage($chatroom);
             
             return $chatroom;
         });
