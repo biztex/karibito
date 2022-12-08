0a25b15d0913ed7f691b84f8a4723b83e9fac731
iimi <toebisu.iimi@gmail.com>
2022-12-08 17:28:03 +0900

    フッターデザイン変更

--- resources/views/components/footer/bottom.blade.php
+++ resources/views/components/footer/bottom.blade.php
@@ -1,12 +1,97 @@
 <div class="bottom">
     <div class="inner">
         <div class="serviceLinks">
-            <a href="{{ route('company') }}">運営会社</a>
-            {{-- <a href="#">採用情報</a>
-            <a href="#">約款特定</a> --}}
+            <div class=footer_list1>
+                <ul>
+                    <li><span class="footerlist_text"><a href="#">サービスを探す</a><span></li>
+                    <li><a href="#">家事</a></li>
+                    <li><a href="#">修理組み立て</a></li>
+                    <li><a href="#">ペット</a></li>
+                    <li><a href="#">高齢者向け</a></li>
+                    <li><a href="#">乗り物</a></li>
+                    <li><a href="#">引越し</a></li>
+                    <li><a href="#">趣味・習い事</a></li>
+                    <li><a href="#">美容・ファッション</a></li>        
+                    <li><a href="#">写真動画制作</a></li>
+                    <li><a href="#">その他</a></li>
+                    <li><a href="#">インテリア</a></li>
+                    <li><a href="#">デザイン</a></li>
+                    <li><a href="#">パソコン</a></li>
+                    <li><a href="#">ビジネスサポート</a></li>
+                    <li><a href="#">冠婚葬祭</a></li>
+                    <li><a href="#">料理</a></li>
+                    <li><a href="#">恋愛・結婚</a></li>        
+                    <li><a href="#">体験・アクティビティ</a></li>
+                    <li><a href="#">出張サービス</a></li>
+                    <li><a href="#">調べもの</a></li>
+                    <li><a href="#">レンタル</a></li>
+                </ul>
+            </div>
+            <div class=footer_list1>
+                <ul>
+                    <li><span class="footerlist_text"><a href="#">リクエストを探す</a><span></li>
+                    <li><a href="#">家事</a></li>
+                    <li><a href="#">修理組み立て</a></li>
+                    <li><a href="#">ペット</a></li>
+                    <li><a href="#">高齢者向け</a></li>
+                    <li><a href="#">乗り物</a></li>
+                    <li><a href="#">引越し</a></li>
+                    <li><a href="#">趣味・習い事</a></li>
+                    <li><a href="#">美容・ファッション</a></li>        
+                    <li><a href="#">写真動画制作</a></li>
+                    <li><a href="#">その他</a></li>
+                    <li><a href="#">インテリア</a></li>
+                    <li><a href="#">デザイン</a></li>
+                    <li><a href="#">パソコン</a></li>
+                    <li><a href="#">ビジネスサポート</a></li>
+                    <li><a href="#">冠婚葬祭</a></li>
+                    <li><a href="#">料理</a></li>
+                    <li><a href="#">恋愛・結婚</a></li>        
+                    <li><a href="#">体験・アクティビティ</a></li>
+                    <li><a href="#">出張サービス</a></li>
+                    <li><a href="#">調べもの</a></li>
+                    <li><a href="#">レンタル</a></li>
+                </ul>
+            </div>
+            <div class=footer_list1>
+                <ul>
+                    <li><span class="footerlist_text"><a href="#">ヘルプ</a><span></li>
+                    <li><a href="#">カリビト攻略法</a></li>
+                    <li><a href="#">カリビトガイド</a></li>
+                    <li><a href="#">ヨクアル質問</a></li>
+                    <li><a href="#">運営会社について</a></li>
+                    <li><a href="#">個人情報の取り扱いについて</a></li>
+                    <li><a href="#">特定商取引法ニ基づく表記</a></li>
+                    <li><a href="#">利用規定</a></li>
+                    <li><a href="#">カテゴリ項目追加依頼</a></li>        
+                    <li><a href="{{ route('contact') }}">お問い合わせ</a></li>
+                </ul>
+            </div>
+            <div class=footer_list1>
+                <ul>
+                    <li><span class="footerlist_text"><a href="#">マイページ</a><span></li>
+                    <li><a href="#">お気に入り</a></li>
+                    <li><a href="#">過去の取引</a></li>
+                    <li><a href="#">掲載内容一覧</a></li>
+                    <li><a href="#">決済履歴</a></li>
+                    <li><a href="#">ポイント取引・利用履歴</a></li>
+                    <li><a href="#">フォロー・フォロワー</a></li>
+                    <li><a href="#">プロモーション・お知らせ</a></li>
+                    <li><a href="#">カリビト知恵袋</a></li>        
+                    <li><a href="#">会員情報</a></li>
+                    <li><a href="#">掲載内容の下書き</a></li>
+                    <li><a href="#">見積書の作成・管理</a></li>
+                    <li><a href="#">履歴書の作成・管理</a></li>
+                    <li><a href="#">お知らせ機能の設定</a></li>
+                </ul>
+                <a href="#" target="_blank" ><img src="/img/common/footer_twitter.png" alt="Twitter" class="footer_twitter"></a>
+            </div>
+
+            
+            <!--<a href="#">約款特定</a> 
             <a href="{{ route('notation') }}">商取引法に基づく表示</a>
             <a href="#">よくある質問</a>
-            <a href="{{ route('contact') }}">お問い合わせ</a>
+            <a href="{{ route('contact') }}">お問い合わせ</a>-->
         </div>
         <div class="sns sp">
             <a href="#" target="_blank"><img src="/img/common/ico_facebook.png" alt="Facebook"></a>
@@ -14,6 +99,7 @@
             <a href="#" target="_blank"><img src="/img/common/ico_instragram.png" alt="Instragram"></a>
             <a href="#" target="_blank"><img src="/img/common/ico_line.png" alt="LINE"></a>
         </div>
-        <p id="copyright">©{{ now()->year }} karibito, Inc.</p>
+       
     </div>
+    <p id="copyright">©{{ now()->year }} karibito, Inc.</p>
 </div>
