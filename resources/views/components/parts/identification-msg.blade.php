961f6c468f5891f40d062dd30b4ded2c93cbbe13
iimi <toebisu.iimi@gmail.com>
2023-01-19 13:38:12 +0900

    再調整

--- resources/views/components/parts/identification-msg.blade.php
+++ resources/views/components/parts/identification-msg.blade.php
@@ -1,7 +1,7 @@
 @if(empty(Auth::user()->userProfile->identification_path))
     <div class="unregisteredP js-unregisteredP">
         <div> </div>
-        <div><img src="./img/common/ico_check.png">{{-- サービスの登録には --}}本人確認未完了<a href="#fancybox_register"  class="fancybox fancybox_register">【登録する】</a></div>
+        <div><img src="./img/common/ico_check.png" style="width:20px; vertical-align:text-top;">{{-- サービスの登録には --}}本人確認未完了<a href="#fancybox_register"  class="fancybox fancybox_register">【登録する】</a></div>
         <div class="pop_close">×</div>
     </div>
 @endif
