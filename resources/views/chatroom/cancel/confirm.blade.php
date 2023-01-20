2d34173b996eee1137671330004cd32b8fbd3926
tanaka <tl9721lt@gmail.com>
2023-01-20 14:50:04 +0900

    キャンセル画面の修正

--- resources/views/chatroom/cancel/confirm.blade.php
+++ resources/views/chatroom/cancel/confirm.blade.php
@@ -19,29 +19,39 @@
 					<x-parts.chatroom-step :value="$purchase->chatroom"/>
 
 					<div class="cancelTitle">
-						<h2>この取引を本当にキャンセルしますか？</h2>
-						<p>キャンセル理由</p>
+						<h2>このお取引を本当にキャンセルしますか？</h2>
 					</div>
+
 					<form id="form" action="" method="post">
 						@csrf
 						<input type="hidden" name="purchase_id" value="{{$request->purchase_id}}">
+                        <div class="cancelReason">
+                            <p>キャンセル理由：</p>
+                            <div class="cancelRea"><p class="reason">
+                                    @for($i=1; $i<7; $i++)
+                                        @if($request['reason'.$i] == "on")
+                                            {{App\Models\PurchasedCancel::REASON[$i]}}<br/>
+                                            <input type="hidden" name="reason{{$i}}" value="on">
+                                        @endif
+                                    @endfor
+                                </p>
+                            </div>
+                        </div>
+                        <div class="evaluation">
+                            <p>キャンセル理由の詳細</p>
+                            <textarea readonly>{{ old('text', $request->text) }}</textarea>
+                            <input type="hidden" name="text" value="{{ $request->text }}">
+                        </div>
 
-						<div class="cancelRea"><p class="reason">
-							@for($i=1; $i<7; $i++)
-								@if($request['reason'.$i] == "on")
-									{{App\Models\PurchasedCancel::REASON[$i]}}<br/>
-									<input type="hidden" name="reason{{$i}}" value="on">
-								@endif
-							@endfor
-							</p>
-							<p>{!! nl2br(e($request->text)) !!}</p>
-							<input type="hidden" name="text" value="{{ $request->text }}">
-						</div>
 						<div class="warnNotes mt50">
-							<p class="danger">キャンセルについて</p>
-							<p>カリビトではキャンセルについて罰則はありません。</p>
-							<p class="danger02">しかし、相手の方はあなたを信頼して契約しています。<br>よほどの理由がない限りは安易にキャンセルしないようにしましょう。</p>
-							<p class="note">※キャンセル申請をするとマイページにキャンセル申請の数が表示されます</p>
+							<p class="danger">！キャンセルについて</p>
+                            <p>キャンセルは原則として双方の合意が必要です。</p>
+                            <p>トラブル防止のため、必ず明確な理由をお伝えください。</p>
+                            <p>相手の方がキャンセル申請に「承認」も「再交渉」も選択しないまま72時間が経過した場合は、自動的に「承認」となります。</p>
+                            <p>その後、双方の評価入力をもってキャンセルは完了します。</p>
+{{--							<p>カリビトではキャンセルについて罰則はありません。</p>--}}
+{{--							<p class="danger02">しかし、相手の方はあなたを信頼して契約しています。<br>よほどの理由がない限りは安易にキャンセルしないようにしましょう。</p>--}}
+{{--							<p class="note">※キャンセル申請をするとマイページにキャンセル申請の数が表示されます</p>--}}
 						</div>
 						<div class="functeBtns">
 							<input type="submit" formaction="{{ route('cancel.store' ,$purchase->id) }}" class="blue loading-disabled" value="上記内容でキャンセルを申請する">
--- resources/views/chatroom/cancel/create.blade.php
+++ resources/views/chatroom/cancel/create.blade.php
@@ -32,37 +32,41 @@
 					</ul>
 
 					<div class="cancelTitle">
-						<h2>この取引を本当にキャンセルしますか？</h2>
-						<p>※キャンセルの場合は理由を下記の中からお選びください。</p>
+						<h2>このお取引きを本当にキャンセルしますか？</h2>
 					</div>
 					<form id="form" action="" method="">
 					@csrf
 						<input type="hidden" name="purchase_id" value="{{$purchase->id}}">
 						@error('reason1')<div class="alert alert-danger">{{ $message }}</div>@enderror
+                        <p>▸ 該当するキャンセル理由をチェックしてください。（必須）</p>
 						<ul class="cancelRea checkChoice">
 							@if( old('purchase_id') == null && $request->purchase_id == null )
 								<li><label><input type="checkbox" name="reason1" checked>{{App\Models\PurchasedCancel::REASON[1]}}</label></li>
 							@else
-								<li><label><input type="checkbox" name="reason1" @if(old('reason1', $request['reason1']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[1]}}</label></li>								
+								<li><label><input type="checkbox" name="reason1" @if(old('reason1', $request['reason1']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[1]}}</label></li>
 							@endif
-								<li><label><input type="checkbox" name="reason2" @if(old('reason2', $request['reason2']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[2]}}</label></li>								
-								<li><label><input type="checkbox" name="reason3" @if(old('reason3', $request['reason3']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[3]}}</label></li>								
-								<li><label><input type="checkbox" name="reason4" @if(old('reason4', $request['reason4']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[4]}}</label></li>								
-								<li><label><input type="checkbox" name="reason5" @if(old('reason5', $request['reason5']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[5]}}</label></li>								
-								<li><label><input type="checkbox" name="reason6" @if(old('reason6', $request['reason6']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[6]}}</label></li>								
+								<li><label><input type="checkbox" name="reason2" @if(old('reason2', $request['reason2']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[2]}}</label></li>
+								<li><label><input type="checkbox" name="reason3" @if(old('reason3', $request['reason3']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[3]}}</label></li>
+								<li><label><input type="checkbox" name="reason4" @if(old('reason4', $request['reason4']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[4]}}</label></li>
+								<li><label><input type="checkbox" name="reason5" @if(old('reason5', $request['reason5']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[5]}}</label></li>
+								<li><label><input type="checkbox" name="reason6" @if(old('reason6', $request['reason6']) == "on")checked @endif>{{App\Models\PurchasedCancel::REASON[6]}}</label></li>
 						</ul>
 						<div class="evaluation">
 							<div class="write">
 								@error('text')<div class="alert alert-danger" style="font-weight:normal;">{{ $message }}</div>@enderror
-								<p>キャンセル理由の詳細をご記入ください</p>
+								<p>キャンセル理由の詳細をご記入ください<span>（必須）</span></p>
 								<textarea name="text">{{ old('text', $request->text) }}</textarea>
 							</div>
 						</div>
 						<div class="warnNotes mt50">
-							<p class="danger">キャンセルについて</p>
-							<p>カリビトではキャンセルについて罰則はありません。</p>
-							<p class="danger02">しかし、相手の方はあなたを信頼して契約しています。<br>よほどの理由がない限りは安易にキャンセルしないようにしましょう。</p>
-							<p class="note">※キャンセル申請をするとマイページにキャンセル申請の数が表示されます</p>
+							<p class="danger">！キャンセルについて</p>
+                            <p>キャンセルは原則として双方の合意が必要です。</p>
+                            <p>トラブル防止のため、必ず明確な理由をお伝えください。</p>
+                            <p>相手の方がキャンセル申請に「承認」も「再交渉」も選択しないまま72時間が経過した場合は、自動的に「承認」となります。</p>
+                            <p>その後、双方の評価入力をもってキャンセルは完了します。</p>
+{{--							<p>カリビトではキャンセルについて罰則はありません。</p>--}}
+{{--							<p class="danger02">しかし、相手の方はあなたを信頼して契約しています。<br>よほどの理由がない限りは安易にキャンセルしないようにしましょう。</p>--}}
+{{--							<p class="note">※キャンセル申請をするとマイページにキャンセル申請の数が表示されます</p>--}}
 						</div>
 						<div class="functeBtns">
 							<input type="submit" formaction="{{ route('cancel.confirm', $purchase->id) }}" formmethod="post" class="blue loading-disabled" value="キャンセル手続きに進む">
