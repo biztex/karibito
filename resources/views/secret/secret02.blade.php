23b92189dde6930661261da144364fc87672d473
yuji yamasaki <yujiyamasakii@gmail.com>
2023-02-08 09:57:58 +0900

    カリビト チャットのコツ 文言変更

--- resources/views/secret/secret02.blade.php
+++ resources/views/secret/secret02.blade.php
@@ -17,19 +17,19 @@
 						<div class="secretBox">
 							<p class="secretHd"><img src="img/secret/hd_secret.svg" alt="カリビトチャットのコツ"></p>
 							<p class="secretImg"><img src="img/secret/img_secret02.png" alt=""></p>
-							<p class="secretStep">STEP 2</p>
-							<p class="secretTitle"><span>「相手が何を求めているのか」<br>カリビトチャットを使ってヒアリングできていますか？</span></p>
-							<p class="secretTxt">まず相談をする前に、依頼に関する情報は十分に集まっているか確認しましょう。一度あなたの中で整理をしてみて相手の方は何を必要としているかを理解する事を考えてみましょう。</p>
+							<p class="secretStep">point 2</p>
+							<p class="secretTitle"><span>メッセージには素早く反応しましょう！</span></p>
+							<p class="secretTxt">届いたチャットには、出来る限り迅速に返信しましょう。迅速に返信することで、やり取りが円滑に進むだけでなく、お互いの信頼性もアップします。もし、すぐに確かな返信ができない場合は、「承知いたしました。〇時までにご返信いたします。」と一言送ることで、相手に安心してもらえます。</p>
+							<p class="secretTxt">チャット内のやり取りや通知は、メールで受け取ることができます。通知によっては返信期限が設定されているものもあるので、「うっかり見過ごしてしまった」ということがないよう、日頃から通知設定を確認しておきましょう。通知の設定は、マイページの会員情報から行っていただけます。</p>
 						</div>
 						<div class="secretPager">
 							<div class="secretPagerPrev"><a href="{{ route('secret01') }}">前へ</a></div>
 							<select onchange="document.location.href=this.options[this.selectedIndex].value;">
-							<option value="{{ route('secret01') }}">1/6</option>
-								<option value="{{ route('secret02') }}" selected>2/6</option>
-								<option value="{{ route('secret03') }}">3/6</option>
-								<option value="{{ route('secret04') }}">4/6</option>
-								<option value="{{ route('secret05') }}">5/6</option>
-								<option value="{{ route('secret06') }}">6/6</option>
+							<option value="{{ route('secret01') }}">1/5</option>
+								<option value="{{ route('secret02') }}" selected>2/5</option>
+								<option value="{{ route('secret03') }}">3/5</option>
+								<option value="{{ route('secret04') }}">4/5</option>
+								<option value="{{ route('secret05') }}">5/5</option>
 							</select>
 							<div class="secretPagerNext"><a href="{{ route('secret03') }}">次へ</a></div>
 						</div>
