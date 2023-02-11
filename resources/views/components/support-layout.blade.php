91c20224d8ee7831fb380675adefd51d2d2d0897
yuji yamasaki <yujiyamasakii@gmail.com>
2023-02-11 16:06:51 +0900

    ヘッダー、フッター統一

--- resources/views/components/support-layout.blade.php
+++ resources/views/components/support-layout.blade.php
@@ -1,18 +1,7 @@
 <x-app>
     <body>
         <div id="wrapper" class="st2 bg02">
-            <header>
-                <div id="header" class="">
-                    <div id="headerLinks">
-                        <div class="inner">
-                            <div class="item">
-                                <h1 id="headerLogo"><a href="{{ route('home') }}"><img class="pc" src="/img/common/logo.svg" alt="LOGO"><img class="sp" src="/img/common/logo_sp.svg" alt="LOGO"></a></h1>
-                            </div>
-                            <p class="custTit">CUSTOMER SUPPORT</p>
-                        </div>
-                    </div><!-- /.headLinks -->
-                </div><!-- /#header -->
-            </header>
+            @include('components.header')
             <x-parts.ban-msg/>
 
             {{$slot}}
