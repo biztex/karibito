043d4168dbeaa42f333c5d6d0dfbcd5ae1ab0fc1
s-iwkami5 <siwakami@minna-systems.co.jp>
2023-02-14 19:42:44 +0900

    バグ修正、ログインしていない人はシェアできない

--- resources/views/components/parts/invitation-code.blade.php
+++ resources/views/components/parts/invitation-code.blade.php
@@ -4,14 +4,14 @@
             <th>あなたの招待コードURL</th>
             <td>
                 <div class="copy">
-                    <input type="text" class="url" id="copyInput" value="https://1111111">
+                    <input type="text" readonly class="url" id="copyInput" value="https://1111111">
                     <span class="btn" onclick="copy()">コピーする</span>
                 </div>
             </td>
         </tr>
         <tr>
             <th>あなたの招待コード</th>
-            <td><input type="text" class="small" value="{{Auth::user()->userProfile->my_code}}"></td>
+            <td><input type="text" readonly class="small" value="{{Auth::user()->userProfile->my_code}}"></td>
         </tr>
     </table>
 </form>
