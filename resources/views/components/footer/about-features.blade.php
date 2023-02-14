00cbe4d205173d4e1ea4021024545231debfa180
yuji yamasaki <yujiyamasakii@gmail.com>
2023-02-14 11:52:45 +0900

    フッター、カリビトの特徴修正

--- resources/views/components/footer/about-features.blade.php
+++ resources/views/components/footer/about-features.blade.php
@@ -2,21 +2,21 @@
     <div class="inner">
         <h2 class="hdM">カリビトの特徴</h2>
         <div class="slider">
-            <img src="/img/feature/20248.svg" alt="">
-            <img src="/img/feature/20250.svg" alt="">
-            <img src="/img/feature/20252.svg" alt="">
-            <img src="/img/feature/20254.svg" alt="">
-            <img src="/img/feature/20257.svg" alt="">
-            <img src="/img/feature/20259.svg" alt="">
-            <img src="/img/feature/20261.svg" alt="">
-            <img src="/img/feature/20266.svg" alt="">
-            <img src="/img/feature/20269.svg" alt="">
-            <img src="/img/feature/20271.svg" alt="">
-            <img src="/img/feature/20273.svg" alt="">
-            <img src="/img/feature/20275.svg" alt="">
-            <img src="/img/feature/20279.svg" alt="">
-            <img src="/img/feature/20280.svg" alt="">
-            <img src="/img/feature/20281.svg" alt="">
+            <img src="/img/feature/feature1.svg" alt="カリビトチャット">
+            <img src="/img/feature/feature2.svg" alt="検索機能">
+            <img src="/img/feature/feature3.svg" alt="カリビト知恵袋">
+            <img src="/img/feature/feature4.svg" alt="豊富なカテゴリー＆カテゴリー追加機能">
+            <img src="/img/feature/feature5.svg" alt="シェア機能でポイント獲得">
+            <img src="/img/feature/feature6.svg" alt="お見積書・経歴書・NDA（機密保持契約）等の入力フォーム機能">
+            <img src="/img/feature/feature7.svg" alt="カリビト評価">
+            <img src="/img/feature/feature8.svg" alt="使いやすいマイページ機能">
+            <img src="/img/feature/feature9.svg" alt="安心安全なシステム">
+            <img src="/img/feature/feature10.svg" alt="作品集をアピール出来るポートフォリオ機能">
+            <img src="/img/feature/feature11.svg" alt="カリビトブログ">
+            <img src="/img/feature/feature12.svg" alt="対面・非対面のどちらにも対応">
+            <img src="/img/feature/feature13.svg" alt="手数料が安い">
+            <img src="/img/feature/feature14.svg" alt="シンプルで見やすいデザイン">
+            <img src="/img/feature/feature15.svg" alt="本人確認できる書類が豊富">
         </div>
     </div>
 </div>
