a9ab1d0caf65ea88b99f92107ee5986d09537bcb
toebisu003 <system003@toebisu.jp>
2023-02-22 00:01:04 -0800

    SUPPORT＞よくあるご質問 開閉の対応

--- resources/views/components/header-support.blade.php
+++ resources/views/components/header-support.blade.php
@@ -3,7 +3,7 @@
         <div id="headerLinks">
             <div class="inner">
                 <div class="item">
-                    <p class="headerLogo"><a href="{{ route('home') }}"><img class="pc" src="/img/common/logo.svg" alt="LOGO"><img class="sp" src="/img/common/logo_sp.svg" alt="LOGO"></a></p>
+
                 </div>
                 <p class="custTit">CUSTOMER SUPPORT</p>
             </div>
--- resources/views/support/support.blade.php
+++ resources/views/support/support.blade.php
@@ -4,9 +4,6 @@
 			<div class="mypageWrap supportWrap detailStyle">
 				<div class="inner inner03">
 					<div class="supportTop">
-						<div class="search">
-							<input type="text" placeholder="Search"><input type="submit" class="btn" value="">
-						</div>
 						<ul class="anchors">
 							<li class="biggerlink">
 								<p class="tit">FAQ</p>
@@ -15,7 +12,7 @@
 							</li>
 							<li class="biggerlink">
 								<p class="tit">初めての方へ</p>
-								<a href="{{ route('guide') }}" class="ico_support02" target="_blank" rel="noopener noreferrer"></a>
+								<a href="{{ route('guide') }}" class="ico_support02" rel="noopener noreferrer"></a>
 								<p>初心者ガイド</p>
 							</li>
 							<li class="biggerlink">
@@ -25,136 +22,86 @@
 							</li>
                             <li class="biggerlink">
                                 <p class="tit">運営会社</p>
-                                <a href="{{ route('company') }}" class="ico_support04" target="_blank" rel="noopener noreferrer"></a>
+                                <a href="{{ route('company') }}" class="ico_support04" rel="noopener noreferrer"></a>
                                 <p>カリビトについて</p>
                             </li>
 							<li class="biggerlink">
 								<p class="tit">カリビトガイド</p>
-								<a class="ico_support05" target="_blank" rel="noopener noreferrer"></a>
+								<a href="{{ route('karibitoguide') }}" class="ico_support05" rel="noopener noreferrer"></a>
 								<p>カリビトのご利用方法 </p>
 							</li>
 						</ul>
 					</div>
-					<div class="mypageSec02">
-						<h2 class="mypageHd02">お問い合わせの多い質問<a href="support_detail.html" class="more">もっと見る</a></h2>
-						<ul class="mypageUl02">
-							<li>
-								<a href="support_detail.html">
-									<dl>
-										<dt><img src="img/mypage/img_notice01.png" alt=""></dt>
-										<dd>
-											<p class="txt">事務局から個別メッセージ「ログイン通知」</p>
-											<p class="time">0時間前</p>
-										</dd>
-									</dl>
-								</a>
-							</li>
-							<li>
-								<a href="support_detail.html">
-									<dl>
-										<dt><img src="img/mypage/img_notice01.png" alt=""></dt>
-										<dd>
-											<p class="txt">お知らせ内容が入りますお知らせ内容が入ります</p>
-											<p class="time">0時間前</p>
-										</dd>
-									</dl>
-								</a>
-							</li>
-							<li>
-								<a href="support_detail.html">
-									<dl>
-										<dt><img src="img/mypage/img_notice01.png" alt=""></dt>
-										<dd>
-											<p class="txt">お知らせ内容が入りますお知らせ内容が入ります</p>
-											<p class="time">0時間前</p>
-										</dd>
-									</dl>
-								</a>
-							</li>
-							<li>
-								<a href="support_detail.html">
-									<dl>
-										<dt><img src="img/mypage/img_notice01.png" alt=""></dt>
-										<dd>
-											<p class="txt">お知らせ内容が入りますお知らせ内容が入ります</p>
-											<p class="time">0時間前</p>
-										</dd>
-									</dl>
-								</a>
-							</li>
-							<li>
-								<a href="support_detail.html">
-									<dl>
-										<dt><img src="img/mypage/img_notice01.png" alt=""></dt>
-										<dd>
-											<p class="txt">お知らせ内容が入りますお知らせ内容が入ります</p>
-											<p class="time">0時間前</p>
-										</dd>
-									</dl>
-								</a>
-							</li>
-						</ul>
-					</div>
 					<div class="optional" id="support01">
-						<h2 class="mypageHd02"><span><img class="ico" src="img/support/ico_quest_circle.svg">よくあるご質問（FAQ）</span><a href="{{ route('support_detail') }}" class="more">もっと見る</a></h2>
+						<h2 class="mypageHd02"><span><img class="ico" src="img/support/ico_quest_circle.svg">よくあるご質問（FAQ）</span></h2>
 						<p class="hdM">お問い合わせの多い質問</p>
-						<ul class="">
-							<li>
-								<p class="quest"><span>仕事にエントリーするには？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
-							</li>
-							<li>
-								<p class="quest"><span>領収書の発行方法は？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
-							</li>
-							<li>
-								<p class="quest"><span>本契約後の金額変更方法は？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
-							</li>
-							<li>
-								<p class="quest"><span>報酬の受け取り方は？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
-							</li>
-							<li>
-								<p class="quest"><span>決済方法は？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
-							</li>
-						</ul>
+                        <ul class="toggleWrapPC">
+                            <li>
+                                <p class="quest toggleBtn"><span>仕事にエントリーするには？</span><span class="more">回答を見る</span></p>
+                                <p class="answer toggleBox">ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                            </li>
+                            <li>
+                                <p class="quest toggleBtn"><span>領収書の発行方法は？</span><span class="more">回答を見る</span></p>
+                                <p class="answer toggleBox">ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                            </li>
+                            <li>
+                                <p class="quest toggleBtn"><span>本契約後の金額変更方法は？</span><span class="more">回答を見る</span></p>
+                                <p class="answer toggleBox">ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                            </li>
+                            <li>
+                                <p class="quest toggleBtn"><span>報酬の受け取り方は？</span><span class="more">回答を見る</span></p>
+                                <p class="answer toggleBox">ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                            </li>
+                            <li>
+                                <p class="quest toggleBtn"><span>決済方法は？</span><span class="more">回答を見る</span></p>
+                                <p class="answer toggleBox">ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                            </li>
+                        </ul>
 						<p class="hdM">最新の質問</p>
-						<ul class="toggleWrapPC">
-							<li>
-								<p class="quest"><span>仕事にエントリーするには？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
-							</li>
-							<li>
-								<p class="quest"><span>領収書の発行方法は？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
-							</li>
-							<li>
-								<p class="quest"><span>本契約後の金額変更方法は？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
-							</li>
-							<li>
-								<p class="quest"><span>報酬の受け取り方は？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
-							</li>
-							<li>
-								<p class="quest"><span>決済方法は？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
-							</li>
-						</ul>
+                        <ul class="toggleWrapPC">
+                            <li>
+                                <p class="quest toggleBtn"><span>仕事にエントリーするには？</span><span class="more">回答を見る</span></p>
+                                <p class="answer toggleBox">ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                            </li>
+                            <li>
+                                <p class="quest toggleBtn"><span>領収書の発行方法は？</span><span class="more">回答を見る</span></p>
+                                <p class="answer toggleBox">ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                            </li>
+                            <li>
+                                <p class="quest toggleBtn"><span>本契約後の金額変更方法は？</span><span class="more">回答を見る</span></p>
+                                <p class="answer toggleBox">ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                            </li>
+                            <li>
+                                <p class="quest toggleBtn"><span>報酬の受け取り方は？</span><span class="more">回答を見る</span></p>
+                                <p class="answer toggleBox">ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                            </li>
+                            <li>
+                                <p class="quest toggleBtn"><span>決済方法は？</span><span class="more">回答を見る</span></p>
+                                <p class="answer toggleBox">ダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+                            </li>
+                        </ul>
 					</div>
 					<div class="optional" id="support02">
 						<h2 class="mypageHd02"><span><img class="ico" src="img/support/ico_shield.svg">初めての方へ</span></h2>
 						<ul class="toggleWrapPC">
 							<li>
-								<p class="quest"><span>カリビトとは？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
+								<p class="quest toggleBtn"><span>カリビトとは？</span><span class="more">回答を見る</span></p>
 								<p class="answer toggleBox">カリビトとはカリビトとはカリビトとはカリビトとはカリビトとはカリビトとはカリビトとはカリビトとはカリビトとはカリビトとはカリビトとはカリビトとは</p>
 							</li>
 							<li>
-								<p class="quest"><span>新規会員登録（無料）するには？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
+								<p class="quest toggleBtn"><span>新規会員登録（無料）するには？</span><span class="more">回答を見る</span></p>
 								<p class="answer toggleBox">新規会員登録（無料）するには新規会員登録（無料）するには新規会員登録（無料）するには新規会員登録（無料）するには新規会員登録（無料）するには新規会員登録（無料）するには新規会員登録（無料）するには新規会員登録（無料）するには新規会員登録（無料）するには新規会員登録（無料）するには</p>
 							</li>
 							<li>
-								<p class="quest"><span>プロフィール編集をするには？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
+								<p class="quest toggleBtn"><span>プロフィール編集をするには？</span><span class="more">回答を見る</span></p>
 								<p class="answer toggleBox">プロフィール編集をするにはプロフィール編集をするにはプロフィール編集をするにはプロフィール編集をするには</p>
 							</li>
 							<li>
-								<p class="quest"><span>取引の流れは？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
+								<p class="quest toggleBtn"><span>取引の流れは？</span><span class="more">回答を見る</span></p>
 								<p class="answer toggleBox">取引の流れは取引の流れは取引の流れは取引の流れは取引の流れは取引の流れは</p>
 							</li>
 							<li>
-								<p class="quest"><span>利用料はかかる？</span><a href="{{ route('support_detail') }}" class="more">回答を見る</a></p>
+								<p class="quest toggleBtn"><span>利用料はかかる？</span><span class="more">回答を見る</span></p>
 								<p class="answer toggleBox">利用料はかかる利用料はかかる利用料はかかる利用料はかかる利用料はかかる利用料はかかる利用料はかかる</p>
 							</li>
 						</ul>
