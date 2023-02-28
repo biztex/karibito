9e9782fda1b9b26490f0da26500249dab2fcde4c
yuji yamasaki <yujiyamasakii@gmail.com>
2023-02-28 13:30:18 +0900

    会社概要 改修

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -16,45 +16,54 @@
 				<div class="companyStep">
 					<div class="inner">
 						<div class="part policy companyAnchorTarget companyAnchorTarget01" id="company01">
-							<h2 class="title"><span class="step">01</span><span class="eng">Company policy</span>〜カリビト理念〜</h2>
+							<h2 class="title"><span class="step">01</span><span class="eng">COMPANY POLICY</span>〜カリビト理念〜</h2>
 							<p class="logo"><img src="/img/company/logo_policy.svg" alt="">「 カリビトで街と人を繋げ隊！ 」</p>
 							<p class="mb70">多種多様化する都市部から過疎化が進む地域社会まで、用途に合わせて街と人を<br class="sp">繋げることができるユニークな<br class="pc">プラットフォームを創造していくことが我々の企業価値となる。</p>
 							<div class="img"><img src="/img/company/img_company01.png" srcset="/img/company/img_company01.png 1x, /img/company/img_company01@2x.png 2x" alt=""></div>
 						</div>
 						<div class="part companyAnchorTarget companyAnchorTarget02" id="company02">
-							<h2 class="title"><span class="step">02</span><span class="eng">History</span>〜日本文化の継承から〜</h2>
+							<h2 class="title"><span class="step">02</span><span class="eng">HISTORY</span>〜日本文化の継承から〜</h2>
 							<div class="img"><img src="/img/company/img_company02.png" srcset="/img/company/img_company02.png 1x, /img/company/img_company02@2x.png 2x" alt=""></div>
 							<div class="introdBox">
-								<p>皆さんは年間行事でよくご先祖様やご親族の方々へのお参りに足を運び、ご自身が存在することへの感謝や想いの念を伝えたりするご経験は誰もがあるのでは無いのでしょうか。<br>代表の私自身も父より、そんな日本の風習を絶やさないようにと厳しく言われて育ちました。</p>
-								<p>しかし、今の日本の年齢別人口比率では高齢化がどんどん進んでゆき、お墓へ足を運ぶことすらも大変となり、日本の大事な風習が文化として後世に残していけない時代が来ようとしています。<br>私の両親も高齢となってからは、お墓参りに行くことが困難となり、私がお墓参りに行って掃除やしきびを立て、お供え物をして綺麗になったお墓の画像を携帯に送って、安心してもらっていました。<br>ですが画像は画像であり現地に出向いた時と同じような臨場感を自宅で感じることはできません。<br>そんな状況を見据えて、各業界では、さまざまなサービスが生まれており、実際に現地まで送迎してもらえるサービスや、お墓参りの動画を自宅にいる両親に配信できるシステムや、実際に自宅からお線香をあげてお経を唱えてもらえるものまで、さまざまなサービスを提供している業者があります。<br>ですが、そのような素晴らしいサービスやスキルを提供する業者が存在することは、まだまだ一般の人々までは認知されていない現実を知りました。<br>それなら、さまざまなサービス形態のスキルを皆さんがわかり易く利用することができれば、これからの高齢社会においても、お墓参りという日本伝統の文化はずっと受け継ぐことが可能です。<br>このお墓参り文化がきっかけとなり、提供側とユーザー側を繋げる掛橋になれるようなシェアリングスキルマーケットのプラットフォーム開設からカリビト事業が生まれました。そして、更に街と人を繋げる多くのカテゴリーを増やし、普及していくことが、カリビトの原点となります。</p>
+								<p>皆さんは年間行事でよくご先祖様やご親族の方々へのお参りに足を運び、ご自身が存在することへの感謝や想いの念を伝えたりするご経験は誰もがあるのではないでしょうか。</p>
+								<p>代表の私自身も父より、そんな日本の風習を絶やさないようにと厳しく言われて育ちました。</p>
+								<p>しかし、今の日本の年齢別人口比率では高齢化がどんどん進んでゆき、お墓へ足を運ぶことすらも大変となり、日本の大事な風習が文化として後世に残していけない時代が来ようとしています。</p>
+								<p>私の両親も高齢となってからは、お墓参りに行くことが困難となり、私がお墓参りに行って掃除や、しきびを立て、お供え物をして綺麗になったお墓の画像を両親の携帯に送って、安心してもらっていました。</p>
+								<p>ですが画像は画像であり現地に出向いた時と同じような臨場感を自宅で感じることはできません。</p>
+								<p>そんな状況を見据えて、各業界では、さまざまなサービスが生まれており、実際に現地まで送迎してもらえるサービスや、お墓参りの動画を自宅にいる両親に配信できるシステムや、実際に自宅からでもお線香をあげてお経を唱えてもらえるものまで、さまざまなサービスを提供している業者があります。</p>
+								<p>ですが、そのような素晴らしいサービスやスキルを提供する業者が存在することは、まだまだ一般の人々までは認知されていない現実を知りました。</p>
+								<p>それなら、さまざまなサービス形態のスキルを皆さんがわかり易く利用することができれば、これからの高齢化社会においても、お墓参りという日本伝統の文化はずっと受け継ぐことが可能です。</p>
+								<p>このお墓参り文化がきっかけとなり、提供側とユーザー側を繋げる掛橋になれるようなシェアリングスキルマーケットのプラットフォーム開設からカリビト事業が生まれました。そして、更に街と人を繋げる多くのカテゴリーを増やし、普及していくことが、カリビトの原点となります。</p>
 							</div>
 						</div>
 						<div class="part companyAnchorTarget companyAnchorTarget03" id="company03">
-							<h2 class="title"><span class="step">03</span><span class="eng">Message</span>〜代表挨拶〜</h2>
+							<h2 class="title"><span class="step">03</span><span class="eng">MESSAGE</span>〜代表挨拶〜</h2>
 							<div class="img"><img src="/img/company/img_company03.png" srcset="/img/company/img_company03.png 1x, /img/company/img_company03@2x.png 2x" alt=""></div>
 							<div class="introdBox">
-								<p>カリビトは、日常における、あらゆるスキルを容易にシェアリングできるプラットフォームの構築ですが、そもそも事業開始する5年前に、私がお墓参りの代行業者をスキルシェアする事業を思い立ってからが始まりでした。その後開設に至るまでには、5年の構想期間を経てスタートした事業です。</p>
-								<p>今では世界中で、多くのスキルをシェアリングする時代が到来しています。<br>その一つひとつは、多くの人の日常生活を助け、仕事に取り組み、行政においても活用される機会が増えています。<br>そんな企業や個々のスキルは、何もない所からシェアする機会は生まれません。シェアできるプラットフォームが認知され、それを街の人々がわかり易く利用できてこそ新しい人と街、人と人との出会いや、きっかけが生まれます。そのための掛橋になることが我々が創造するカリビトの使命です。</p>
-								<p>カリビトが「街と人を繋げ隊！」というコンセプトのもとに、町の活性化に繋がるツールになるために、これからも進んで参ります。</p>
+								<p>カリビトの役割は、日常における、あらゆるスキルを容易にシェアリングできるプラットフォームの構築ですが、そもそも事業開始する5年前に、私がお墓参りの代行業者をスキルシェアする事業を思い立ってからが始まりでした。その後開設に至るまでには、5年の構想期間を経てスタートした事業です。</p>
+								<p>今では世界中で、多くのスキルをシェアリングする時代が到来しています。<br>その一つひとつは、多くの人の日常生活を助け、仕事に取り組み、行政においても活用される機会が増えています。</p>
+								<p>そんな企業や個々のスキルは、何もない所からシェアする機会は生まれません。シェアできるプラットフォームが認知され、それを街の人々がわかり易く利用できてこそ、新しい人と街、人と人との出会いや、きっかけが生まれます。そのための掛橋になることが我々が創造するカリビトの使命です。</p>
+								<p>カリビトが「街と人を繋げ隊！」というコンセプトのもとに、街の活性化に繋がるツールになるために、これからも進んで参ります。</p>
 							</div>
 						</div>
 						<div class="part">
-							<h2 class="title"><span class="step">04</span><span class="eng">Skill sharing</span>〜雇用問題と使命〜</h2>
+							<h2 class="title"><span class="step">04</span><span class="eng">SKILL SHARING</span>〜雇用問題と使命〜</h2>
 							<div class="img"><img src="/img/company/img_company04.png" srcset="/img/company/img_company04.png 1x, /img/company/img_company04@2x.png 2x" alt=""></div>
 							<div class="introdBox">
-								<p>労働者不足・機会損失による多種多様な職種・スキルを都心一極集中ではなく地方都市においてもベストシェアリングできるマルチプラットフォームの開発・浸透・共有・成長</p>
+								<p>労働者不足・機会損失による多種多様な職種・スキルを、都心一極集中ではなく地方都市においてもベストシェアリングできるマルチプラットフォームの開発・浸透・共有・成長により雇用拡大を使命とします。</p>
 								<h4 class="tit">Employment issue　〜雇用問題〜</h4>
-								<ul class="issue mb40">
-									<li>1　家族、従業員の生活を守るために稼ぎたくてもスキルの提供や雇用ができなければ守れない</li>
-									<li>2　地域住民の需要に応えるために、コンビニ営業のように深夜でも供給できたり、災害時など地域に貢献できる体制も、人がいなければ運営は成り立たない。</li>
-									<li>3　高齢化社会で、介護者、障害者、生活保護者などを助ける、ヘルパー需要が増える中、人がいなければデイケアできない。</li>
-									<li>4　多産化政策を国が実現しようにも、保育所が足りない、保育士不足なら安心して子供を産む気になれない。</li>
-									<li>5　いわゆる感性やマインドの必要とされる仕事は、AIがとって変われない。そんな人材こそ日本文化の良さなのに、そのスキルを提供できる機会がまだまだ確立されていない。</li>
-									<li>6　労働者の質の低下が引き起こすバイトテロなどの問題を解決するためには、必要とする人材のシェアリングだが、特にその仕組みが地域社会において浸透されてい　　ない。</li>
-									<li>7　医師の不足により病院の減少が始まっていたり、医師の負担による、患者への十分な対応ができない。</li>
-									<li>8　女性の社会への積極的な進出を助けるための、職場雇用を斡旋できるツールが少なく、より多くの情報をマッチングできる仕組みがない。</li>
-									<li>9　専門技術者の定年退職や少子化による人材不足などで、日本のモノづくりを支えることができていない。</li>
-									<li>10　今後のさらなる国籍化による、日本の労働者不足を補うために訪れる海外労働者のための就職斡旋の仕組みが確立されていない。</li>
+								<ul class="issue mb40 issueNumber">
+									<li>家族、従業員の生活を守るために稼ぎたくても、スキルの提供や雇用ができなければ守れない。</li>
+									<li>地域住民の需要に応えるために、コンビニ営業のように深夜でも供給できたり、災害時など地域に貢献できる体制も、人がいなければ運営は成り立たない。</li>
+									<li>高齢化社会で、介護者、障害者、生活保護者などを助ける、ヘルパー需要が増える中、人がいなければデイケアできない。</li>
+									<li>多産化政策を国が実現しようにも、保育所が足りない、保育士不足なら安心して子供を産む気になれない。</li>
+									<li>いわゆる感性やマインドの必要とされる仕事は、AIがとって変われない。そんな人材こそ日本文化の良さなのに、そのスキルを提供できる機会がまだまだ確立されていない。</li>
+									<li>労働者の質の低下が引き起こすバイトテロなどの問題を解決するためには、必要とする人材のシェアリングだが、特にその仕組みが地域社会において浸透されていない。</li>
+									<li>医師の不足により病院の減少が始まっていたり、医師の負担による、患者への十分な対応ができていない。</li>
+									<li>女性の社会への積極的な進出を助けるための、職場雇用を斡旋できるツールが少なく、より多くの情報をマッチングできる仕組みがない。</li>
+									<li>専門技術者の定年退職や少子化による人材不足などで、日本のモノづくりを支えることができていない。</li>
+									<li>今後のさらなる国籍化による、日本の労働者不足を補うために訪れる海外労働者のための就職斡旋の仕組みが確立されていない。</li>
+									<li>世界基準で言うと、紛争地域やテロにより、家や職を失った人達が沢山いるなかで、そんな人達がどの地域でも安心して働けるような仕事を幹旋する仕組みがない。</li>
 								</ul>
 								<p class="tit">Mission　〜使命〜</p>
 								<ul>
@@ -64,93 +73,83 @@
 							</div>
 						</div>
 						<div class="part">
-							<h2 class="title"><span class="step">05</span><span class="eng">Vision</span>〜事業方針〜</h2>
+							<h2 class="title"><span class="step">05</span><span class="eng">VISION</span>〜事業方針〜</h2>
 							<div class="img"><img src="/img/company/img_company05.png" srcset="/img/company/img_company05.png 1x, /img/company/img_company05@2x.png 2x" alt=""></div>
 							<div class="introdBox">
 								<h4 class="tit">・Area activation（地域活性化）</h4>
-								<p>一極集中している首都圏や中核都市から地方都市へUターン・Iターン・Jターンを促す雇用獲得と労働人口不足を補うためのスキルシェアリングを促進させ地域活性化を促す</p>
+								<p class="introdBoxTitUnder">一極集中している首都圏や中核都市から地方都市へUターン・Iターン・Jターンの雇用獲得と労働人口不足を補うためのスキルシェアリングを促進させ地域活性化を促す。</p>
 								<h4 class="tit">・Business strategy（事業戦略）</h4>
-								<p>安心で信頼できる情報を提供するスキルシェアリングのプラットフォームの操作性・利便性・地域浸透性・地域社会と連携してユニークな試みを創出していく。</p>
+								<p class="introdBoxTitUnder">ユニークで信頼できるシステム（プラットフォーム）を地域社会と連携して開発していく。</p>
 							</div>
 						</div>
 						<div class="part">
-							<h2 class="title"><span class="step">06</span><span class="eng">Ares exchange</span>〜地域交流〜</h2>
+							<h2 class="title"><span class="step">06</span><span class="eng">ARES EXCHANGE</span>〜地域交流〜</h2>
 							<div class="img"><img src="/img/company/img_company06.png" srcset="/img/company/img_company06.png 1x, /img/company/img_company06@2x.png 2x" alt=""></div>
 							<div class="introdBox">
-								<p>東京、大阪エリアからスタートして、その後地方都市へと展開し、労働人口の不足を補うためには、一極集中している首都圏や中核都市からの労働者の獲得が必要です。<br>そのために地域社会と連携をして、ビジネス・スキル交流を定期的に開催して参ります。</p>
+								<p>社会問題の解決策の一つの手段として注目されているスキルシェアは、自治体課題の解決や地域経済活性化の手段としても有効であり、積極的に地域社会に普及・活用をさせていく必要があります。</p>
+								<p>カリビトがICTを活用した地域課題を解決するための推進役となり、地域社会におけるスキルシェアの浸透・定着に向けたビジネス・スキル交流を定期的に開催して参ります。</p>
 							</div>
 						</div>
 						<div class="part">
-							<h2 class="title"><span class="step">07</span><span class="eng">Growth</span>〜事業浸透から成長〜</h2>
+							<h2 class="title"><span class="step">07</span><span class="eng">GROWTH</span>〜事業浸透から成長〜</h2>
 							<div class="img"><img src="/img/company/img_company07.png" srcset="/img/company/img_company07.png 1x, /img/company/img_company07@2x.png 2x" alt=""></div>
 							<div class="introdBox">
-								<p>カリビトの利用形態には一般ユーザー側（スキルを購入する人）と、クリエイター（スキルを提供する人）、そして、ユーザーリクエスト（スキルをリクエストする人）の3視点からの利用法があります。<br>しかし、この3視点がバランスよくそれぞれのニーズを満たせなければ、プラットフォームとしては成立しなくなってしまいます。今後、地域社会と、どのように連携しながら浸透ていくかは、カリビトの成長戦略においてお重要となります。</p>
+								<p>カリビトの利用形態には一般ユーザー側（スキルを購入する人）と、クリエイター（スキルを提供する人）、そして、ユーザーリクエスト（スキルをリクエストする人）の3視点からの利用法があります。</p>
+								<p>しかし、この3視点がバランスよくそれぞれのニーズを満たせなければ、プラットフォームとしては成立しなくなってしまいます。今後、地域社会と、どのように連携しながら浸透していくかは、カリビトの成長戦略においても重要となります。</p>
 							</div>
 						</div>
 						<div class="part">
-							<h2 class="title"><span class="step">08</span><span class="eng">Employment change</span>〜雇用機会の拡大〜</h2>
+							<h2 class="title"><span class="step">08</span><span class="eng">EMPLOYMENT CHANGE</span>〜雇用機会の拡大〜</h2>
 							<div class="img"><img src="/img/company/img_company08.png" srcset="/img/company/img_company08.png 1x, /img/company/img_company08@2x.png 2x" alt=""></div>
 							<div class="introdBox">
-								<p>アフターコロナ における世の中は、地域一極集中から、地方分散の時代へと変わろうとしています。それはビジネスだけでなく、日常生活からレジャーにおいてまで、地域活性化に繋がる取り組みを各関連機関が積極的に推進し始めています。<br>そのためには、地方に自然に溶け込むなかで、雇用機会を生み出す一つのきっかけとなります。<br>カリビトが街と人を繋げるプラットフォームツールとなるよう雇用機会拡大の一角を担って参ります。</p>
+								<p>アフターコロナにおける世の中は、地域一極集中から、地方分散の時代へと変わろうとしています。それはビジネスだけでなく、日常生活からレジャーにおいてまで、地域活性化に繋がる取り組みを各関連機関が積極的に推進し始めています。</p>
+								<p>そのためには、地方に自然に溶け込むなかで、雇用情報が入手できたり、スキルシェアを通して雇用の機会が生まれたりすることが、雇用機会を生み出す一つのきっかけとなります。</p>
+								<p>カリビトが街と人を繋げるプラットフォームツールとなるよう雇用機会拡大の一角を担って参ります。</p>
 							</div>
 						</div>
 						<div class="part companyProfile">
-							<h2 class="hdS"><img class="ico" src="/img/company/ico_logo.svg"><span class="eng">Company profile</span><span class="jap">〜会社概要〜</span></h2>
+							<h2 class="hdS"><img class="ico" src="/img/company/ico_logo.svg"><span class="eng">COMPANY PROFILE</span><span class="jap">〜会社概要〜</span></h2>
 							<div class="profile">
 								<table>
-									<tbody><tr>
-										<th>会社名</th>
-										<td>株式会社カリビト</td>
-									</tr>
-									<tr>
-										<th>所在地</th>
-										<td>愛媛県松山市二番町5丁目7</td>
-									</tr>
-									<tr>
-										<th>電話番号</th>
-										<td>089-993-8781</td>
-									</tr>
-									<tr>
-										<th>FAX番号</th>
-										<td>089-993-8781</td>
-									</tr>
-									<tr>
-										<th>URL</th>
-										<td><a href="https://karibito.co.jp/" target="_blank" rel="noopener noreferrer">https://karibito.co.jp/</a></td>
-									</tr>
-									<tr>
-										<th>E-mail</th>
-										<td><a href="mailto:info">info@karibito.co.jp</a></td>
-									</tr>
-									{{-- <tr>
-										<th>定休日</th>
-										<td>テキストが入ります</td>
-									</tr>
-									<tr>
-										<th>登録番号</th>
-										<td>テキストが入ります</td>
-									</tr> --}}
-									{{-- <tr>
-										<th>設立年月日</th>
-										<td>0000年0月0日</td>
-									</tr> --}}
-									<tr>
-										<th>代表者</th>
-										<td>代表取締役　鎌田 哲至</td>
-									</tr>
-									{{-- <tr>
-										<th>資本金</th>
-										<td>0000円</td>
-									</tr> --}}
-									<tr>
-										<th>主要取引先銀行</th>
-										<td>伊予銀行　みずほ銀行</td>
-									</tr>
-									<tr>
-										<th>事業内容</th>
-										<td>スキルマーケット「カリビト」に関する運営・開発・販売</td>
-									</tr>
-								</tbody></table>
+									<tbody>
+										<tr>
+											<th>会社名</th>
+											<td>株式会社カリビト</td>
+										</tr>
+										<tr>
+											<th>設立</th>
+											<td>2022年7月</td>
+										</tr>
+										<tr>
+											<th>所在地</th>
+											<td>〒106-0044 東京都港区東麻布2-35-1 KCビル6F</td>
+										</tr>
+										<tr>
+											<th>代表</th>
+											<td>代表取締役 鎌田 哲至</td>
+										</tr>
+										<tr>
+											<th>URL</th>
+											<td><a href="https://karibito.co.jp/" class="companyProfileMail">https://karibito.co.jp/</a></td>
+										</tr>
+										<tr>
+											<th>お問い合わせ</th>
+											<td><a href="mailto:info@karibito.co.jp" class="companyProfileLink">お問い合わせはこちら</a></td>
+										</tr>
+										<tr>
+											<th>事業内容</th>
+											<td>スキルマーケット「カリビト」に関する運営・開発・販売</td>
+										</tr>
+										<tr>
+											<th>外部リンク</th>
+											<td>
+												<a href="https://twitter.com/karibito_Inc">
+													<img src="https://karibito.co.jp/./img/Twitter.svg" class="contact_icon">
+												</a>
+											</td>
+										</tr>
+									</tbody>
+								</table>
 							</div>
 						</div>
 						<div class="part contact" id="company04">
