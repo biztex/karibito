22506a925acca5eed70a393f75467b183384d1f5
toebisu003 <system003@toebisu.jp>
2023-03-31 01:07:03 -0700

    カリビト ガイド　改修

--- resources/views/support/guide.blade.php
+++ resources/views/support/guide.blade.php
@@ -12,7 +12,7 @@
                     </div>
 
 					<div class="btnTeams">
-						<a href="{{ route('home') }}" class="blue">サービスを探す</a>
+						<a href="/product/index/keyword/search?service_flg=1&keyword=&search_flg=1" class="blue">サービスを探す</a>
 						{{-- @can('identify') --}}
 							<a href="{{ route('post') }}" class="yellow">サービスを投稿する</a>
 						{{-- @endcan --}}
@@ -22,31 +22,32 @@
 			<div class="guideApp">
 				<div class="inner inner03">
 					<h2><span class="hot">カリビトアプリ</span><br class="sp">で何ができる？</h2>
+                    <h2>カリビトとは</h2>
 					<ul>
 						<li>
 							<p class="tit"><span class="point">point1</span>街の助け合いアプリ</p>
 							<div class="img"><img src="img/guide/img_app01.png" srcset="img/guide/img_app01.png 1x, img/guide/img_app01@2x.png 2x" alt="街の助け合いアプリ"></div>
-							<p>カリビトを使うと、アプリを してご 所さん気 に会って、助け合うことができます。〇〇〇から〇〇〇まで、様々なジャンルで助け合いましょう。</p>
+							<p>カリビトは、日常におけるあらゆるスキルをシェアリング出来るプラットフォームです。人と人、街と人を繋ぐ架け橋となるアプリです。</p>
 						</li>
 						<li>
-							<p class="tit"><span class="point">point2</span>サービスの提供</p>
-							<div class="img"><img src="img/guide/img_app02.png" srcset="img/guide/img_app02.png 1x, img/guide/img_app02@2x.png 2x" alt="サービスの提供"></div>
-							<p>自分の得意で誰かの役に立ちたい、気軽におこづかいを稼ぎたい、という人はサービスを提供しましょう。</p>
+							<p class="tit"><span class="point">point2</span>スキルの出品</p>
+							<div class="img"><img src="img/guide/img_app02.png" srcset="img/guide/img_app02.png 1x, img/guide/img_app02@2x.png 2x" alt="スキルの出品"></div>
+							<p>自分の持つスキルやノウハウを収入に繋げたい、誰かの役に立てたいという方はスキルを出品しましょう。</p>
 						</li>
 						<li>
 							<p class="tit"><span class="point">point3</span>リクエスト募集</p>
 							<div class="img"><img src="img/guide/img_app03.png" srcset="img/guide/img_app03.png 1x, img/guide/img_app03@2x.png 2x" alt="リクエスト募集"></div>
-							<p>自分の困りごとを誰かに解決してほしい、という人はリクエストを募集しましょう。</p>
+							<p>探しているスキルが見つからない人は、スキルのリクエストが出来ます。あなたの困りごとを解決してくれる人を募集しましょう。</p>
 						</li>
 						<li>
 							<p class="tit"><span class="point">point4</span>選べる期間</p>
 							<div class="img"><img src="img/guide/img_app04.png" srcset="img/guide/img_app04.png 1x, img/guide/img_app04@2x.png 2x" alt="選べる期間"></div>
-							<p>単発のリクエストと長期のリクエストの2種類のパターンがありますので、ライフスタイルや希望に合ったリクエストを見つけることが可能です。</p>
+							<p>あなたのライフスタイルに合わせたスキルの出品が出来ます。短期・長期、無理のないスケジュールでスキルを提供しましょう。</p>
 						</li>
 						<li>
 							<p class="tit"><span class="point">point5</span>選べる仕事体系</p>
 							<div class="img"><img src="img/guide/img_app05.png" srcset="img/guide/img_app05.png 1x, img/guide/img_app05@2x.png 2x" alt="選べる仕事体系"></div>
-							<p>カリビトでは、「リモート」でのお仕事と「対面」でのお仕事をそれぞれ募集や依頼することができます。インターネットが繋がればOKな仕事と店舗など対面で働く仕事を選択する事ができます。</p>
+							<p>カリビトでは、「対面」と「非対面」両方のスキルを出品・リクエスト出来ます。オンラインでできるお仕事と直接人と関わるお仕事、あなたのスキルに応じて選択できます。</p>
 						</li>
 					</ul>
 				</div>
@@ -62,13 +63,13 @@
 						<li>
 							<a href="#etiquette01" class="buy scroll">
 								<p class="ico"><img src="img/guide/ico_etiquette_point01.svg"></p>
-								<p class="tit">スキルを<span class="cate"><big>購入したい方</big><br class="sp"></span></p>
+								<p class="tit">スキルを<span class="cate"><big>購入したい方へ</big><br class="sp"></span></p>
 							</a>
 						</li>
 						<li>
 							<a href="#etiquette02" class="provid scroll">
 								<p class="ico"><img src="img/guide/ico_etiquette_point02.svg"></p>
-								<p class="tit">はじめまして<span class="cate"><big>出品したい方</big><br class="sp"></span></p>
+								<p class="tit">スキルを<span class="cate"><big>出品したい方へ</big><br class="sp"></span></p>
 							</a>
 						</li>
 					</ul>
@@ -80,26 +81,26 @@
 							<li>
 								<p class="img"><img src="img/guide/img_buyer01.svg" alt=""></p>
                                 <p class="status">【購入前】</p>
-								<p class="tit">★サービスの詳細をしっかりと確認し、疑問や不安点を無くしてから購入しましょう！</p>
+                                <p class="tit starFlex"><span>★</span><span>サービスの詳細をしっかりと確認し、疑問や不安点を無くしてから購入しましょう！</span></p>
 								<p>購入の意思を伝える前に、提供されているサービス内容があなたの求めているものとマッチしているか、まずはサービスページをしっかりと見ましょう。その上で、疑問や要望は購入前にきちんと確認することで、購入後のキャンセルやトラブルの防止になります。</p>
 							</li>
 							<li>
 								<p class="img"><img src="img/guide/img_buyer02.svg" alt=""></p>
                                 <p class="status">【購入後】</p>
-								<p class="tit">★スピーディーな返信を心がけましょう！</p>
+                                <p class="tit starFlex"><span>★</span><span>スピーディーな返信を心がけましょう！</span></p>
 								<p>確認事項に返信がもらえないと、出品者の方は作業を進めることができません。チャットが届いたら、返信はスピーディーにしましょう。もしすぐ返信ができない場合は「ご連絡ありがとうございます。何日までにご連絡します。」と一報を入れることで、お互いに気持ちよく取引を進めることができます。</p>
 							</li>
 							<li>
 								<p class="img"><img src="img/guide/img_buyer03.svg" alt=""></p>
                                 <p class="status">【購入後】</p>
-								<p class="tit">★要望やイメージは分かりやすく伝えましょう！</p>
+								<p class="tit starFlex"><span>★</span><span>要望やイメージは分かりやすく伝えましょう！</span></p>
 								<p>例えば「かわいい感じ」といっても、思い描くイメージは人それぞれです。要望やイメージを伝えるときは、客観的に伝わるかどうかを意識しましょう。</p>
 							</li>
                             <li>
                                 <p class="img"><img src="img/guide/img_buyer04.svg" alt=""></p>
                                 <p class="status">【評価】</p>
-                                <p class="tit">★納品完了後は速やかに評価の入力をしましょう！</p>
-                                <p>カリビトのお取引は、双方の評価が入力された時点で完了となります。せっかく期日内に納品されても、あなたの評価が未入力であれば出品者の方は報酬が受け取れません。納品完了通知に承諾後は、すぐ評価入力を行いましょう。<br>コメントと5段階評価の一部（総合評価）は公開され、他のユーザーも見ることができます。<br>また、あなたの評価は他の方が購入をされる際の判断材料の一つとなります。公正公平で丁寧な入力をしましょう。</p>
+                                <p class="tit starFlex"><span>★</span><span>納品完了後は速やかに評価の入力をしましょう！</span></p>
+                                <p>カリビトのお取引は、双方の評価が入力された時点で完了となります。せっかく期日内に納品されても、あなたの評価が未入力であれば出品者の方は報酬が受け取れません。納品完了通知に承諾後は、すぐ評価入力を行いましょう。<br>また、あなたの評価は他の方が購入をされる際の判断材料の一つとなります。公正公平で丁寧な入力をしましょう。</p>
                             </li>
 						</ul>
 					</div>
@@ -110,32 +111,32 @@
 						<ul>
 							<li>
 								<p class="img"><img src="img/guide/img_provider01.svg" alt=""></p>
-                                <p class="status">【出品時】</p>
-								<p class="tit">★分かりやすい出品ページを作成しましょう！</p>
+                                <p class="status">【交渉中】</p>
+                                <p class="tit starFlex"><span>★</span><span>分かりやすい出品ページを作成しましょう！</span></p>
 								<p>購入者の方の目線で、分かりやすく丁寧な出品ページを心がけましょう。よくある質問は、出品ページに表示させておくことで、お問い合わせのチャットを軽減させることができます。</p>
 							</li>
 							<li>
 								<p class="img"><img src="img/guide/img_provider02.svg" alt=""></p>
-                                <p class="status">【出品時】</p>
-								<p class="tit">★購入者の方のお問い合わせには、迅速かつ丁寧に返信しましょう。</p>
+                                <p class="status">【交渉中】</p>
+                                <p class="tit starFlex"><span>★</span><span>購入者の方のお問い合わせには、迅速かつ丁寧に返信しましょう。</span></p>
 								<p>購入者の方は、あなたからの「サービス提供」通知がなければ購入に進めません。購入の意思の連絡にはすぐ返信するよう心がけ、確認事項に時間を要する場合はきちんとその旨を伝えるなど礼儀正しい対応をしましょう。</p>
 							</li>
 							<li>
 								<p class="img"><img src="img/guide/img_provider03.svg" alt=""></p>
-                                <p class="status">【出品後】</p>
-								<p class="tit">★納期をしっかりと守りましょう！</p>
+                                <p class="status">【契約後】</p>
+                                <p class="tit starFlex"><span>★</span><span>納期をしっかりと守りましょう！</span></p>
 								<p>決められた納期はきちんと守りましょう。決められた納期に間に合わないことは、購入者の方に迷惑をかけるだけでなく、頑張って進めていた作業がキャンセルされる恐れもあります。まずは契約前に必ず納期を確認し、無理のない作業計画を立てましょう。また、進捗などを中間報告をすることで、購入者の方も安心して取引することができます。</p>
 							</li>
 							<li>
 								<p class="img"><img src="img/guide/img_provider04.svg" alt=""></p>
-                                <p class="status">【出品後】</p>
-								<p class="tit">★「納品完了」通知は双方合意の上で送りましょう！</p>
+                                <p class="status">【契約後】</p>
+                                <p class="tit starFlex"><span>★</span><span>「納品完了」通知は双方合意の上で送りましょう！</span></p>
 								<p>特に非対面のサービス提供の場合、チャットだけのやり取りとなりますので、提供するサービス内容に関してお互いが合意しているかを確認することは非常に大切です。一方的な「納品完了」の通知は、購入者の不信感に繋がりますので、気を付けましょう。</p>
 							</li>
                             <li>
                                 <p class="img"><img src="img/guide/img_provider05.svg" alt=""></p>
                                 <p class="status">【評価】</p>
-                                <p class="tit">★忘れずに購入者の評価入力をしましょう！</p>
+                                <p class="tit starFlex"><span>★</span><span>忘れずに購入者の評価入力をしましょう！</span></p>
                                 <p>カリビトのお取引は、双方の評価が入力された時点で終了となります。あなたが購入者の方の評価を入力しないと、取引完了とならず報酬を受け取れません。「評価入力」通知が届きましたら、すぐ入力を行いましょう。<br>評価入力は、公正公平で丁寧な入力を心がけましょう。</p>
                             </li>
 						</ul>
