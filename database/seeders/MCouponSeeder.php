614bb08759873684202197b9fe88a35c6b44077a
s-iwkami5 <siwakami@minna-systems.com>
2023-04-04 19:17:43 +0900

    クーポン画面の編集

--- database/seeders/MCouponSeeder.php
+++ database/seeders/MCouponSeeder.php
@@ -15,8 +15,8 @@ class MCouponSeeder extends Seeder
     public function run()
     {
         $params = [
-            ['name' => '新規会員登録 500円OFFクーポン', 'content' => 'お1人様1回まで', 'discount' => 500, 'min_price' => 5000, 'deadline_period' => '6'],
-            ['name' => 'カリビトアンケート回答 100円OFFクーポン', 'content' => 'お1人様1回まで', 'discount' => 100, 'min_price' => 1000, 'deadline_period' => '6'],
+            ['name' => '新規会員登録 500円OFFクーポン', 'content' => '5,000円以上のお支払いでご利用いただけます。お1人様1回限り使用可能です。ポイント、他のクーポンとの併用はできません', 'discount' => 500, 'min_price' => 5000, 'deadline_period' => '6'],
+            ['name' => 'カリビトアンケート回答 100円OFFクーポン', 'content' => '1,000円以上のお支払いでご利用いただけます。お1人様1回限り使用可能です。ポイント、他のクーポンとの併用はできません', 'discount' => 100, 'min_price' => 1000, 'deadline_period' => '6'],
         ];
         \DB::table('m_coupons')->insert($params);
     }
