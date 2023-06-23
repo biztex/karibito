0b3ddaadcf2ea0cc5821ac9a2e8e917a994fac46
toebisu003 <system003@toebisu.jp>
2023-06-23 20:04:44 +0900

    初回出品登録時に出品者にポイントを付与するロジックの修正

--- app/Http/Controllers/Web/Mypage/ProductController.php
+++ app/Http/Controllers/Web/Mypage/ProductController.php
@@ -81,12 +81,10 @@ class ProductController extends Controller
             $this->product_service->storeProductQuestion($request->all(), $product->id);
             $this->product_service->storeProductLink($request->all(), $product->id);
             $this->product_service->storeImage($request, $product->id);
-
-            // 出品登録時に出品者に毎回ポイントを付与する
-            $this->point_service->getPoint(MPoint::PRODUCT_REGISTRATION, \Auth::id(), $product);
             // 初回出品登録時に出品者にポイントを付与するロジック
-            // if (!UserGetPoint::grantCoupon(\Auth::id(), MPoint::POINT_NAME[MPoint::PRODUCT_REGISTRATION])) {
-            // }
+            if (!UserGetPoint::grantCoupon(\Auth::id(), MPoint::POINT_NAME[MPoint::PRODUCT_REGISTRATION])) {
+                $this->point_service->getPoint(MPoint::PRODUCT_REGISTRATION, \Auth::id(), $product);
+            }
         });
 
         $product = Product::orderBy('created_at', 'desc')->where('user_id', \Auth::id())->first();
@@ -120,7 +118,7 @@ class ProductController extends Controller
         $is_favorite = Favorite::product()->where('reference_id', $product->id)->first();
 
         $url = $this->product_service->getURL($product->id);
-        
+
         $count_favorite = $product->favorites->count(); // いいねの数
 
         $product->view_count += 1; //表示回数をカウント
