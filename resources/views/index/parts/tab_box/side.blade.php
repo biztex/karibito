87271f140fb3544d8f97470df70d6949a1ac7d03
biztex <luxuryman887@gmail.com>
2023-09-11 17:45:50 +0900

    サイドバーの「よくある質問」のリンク修正

--- resources/views/index/parts/tab_box/side.blade.php
+++ resources/views/index/parts/tab_box/side.blade.php
@@ -8,7 +8,7 @@
     <h2>ガイド</h2>
     <ul class="links">
         <li><a href="{{ route('support') }}">ご利用ガイド</a></li>
-        <li><a href="#">よくある質問</a></li>
+        <li><a href="{{ route('support') }}#support01">よくある質問</a></li>
         <li><a href="{{ route('privacy-policy') }}">プライバシーポリシー</a></li>
         <li><a href="{{ route('contact') }}">お問い合わせ</a></li>
     </ul>
